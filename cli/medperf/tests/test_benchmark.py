943e2d76926624a5eb8f5bb9454e050815dcb210
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2021-10-19 19:05:02 -0500

    Test additional models in benchmark

--- cli/medperf/tests/test_benchmark.py
+++ cli/medperf/tests/test_benchmark.py
@@ -10,7 +10,7 @@ def server(mocker):
     server = Server("mock.url")
     patch_server = "medperf.entities.benchmark.Server.{}"
     mocker.patch(patch_server.format("get_benchmark"), side_effect=benchmark_body)
-    mocker.patch(patch_server.format("get_benchmark_models"), return_value=[])
+    mocker.patch(patch_server.format("get_benchmark_models"), return_value=[5, 6, 7])
     return server
 
 
@@ -38,10 +38,19 @@ def test_get_benchmark_retrieves_models_from_server(mocker, server):
     spy.assert_called_once_with(uid)
 
 
-def test_get_benchmark_includes_reference_model_in_models(server):
+def test_benchmark_includes_reference_model_in_models(server):
     # Act
     uid = 1
     benchmark = Benchmark.get(uid, server)
 
     # Assert
     assert benchmark.reference_model in benchmark.models
+
+
+def test_benchmark_includes_additional_models_in_modles(server):
+    # Act
+    uid = 1
+    benchmark = Benchmark.get(uid, server)
+
+    # Assert
+    assert set([5, 6, 7]).issubset(set(benchmark.models))
