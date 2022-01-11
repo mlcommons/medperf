f7d02547cbee93b1f165f61a4bfa17a4c5ca0852
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-01-11 11:09:26 -0500

    Add parameters to cli script

--- cli/cli.sh
+++ cli/cli.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
+while getopts u:p:s: flag
+do
+    case "${flag}" in
+        u) ADMIN_USERNAME=${OPTARG};;
+        p) ADMIN_PASS=${OPTARG};;
+        s) SERVER_URL=${OPTARG};;
+    esac
+done
+ADMIN_USERNAME="${ADMIN_USERNAME:-admin}"
+ADMIN_PASS="${ADMIN_PASS:-admin}"
+SERVER_URL="${SERVER_URL:-http://127.0.0.1:8000}"
+
+echo "Admin username: $ADMIN_USERNAME"
+echo "Admin password: $ADMIN_PASS"
+echo "Server URL: $SERVER_URL"
+
 echo "====================================="
 echo "Cleaning up medperf tmp files"
 echo "====================================="
@@ -13,7 +29,7 @@ tar -xzvf /tmp/mock_chexpert_dset.tar.gz -C /tmp &> /dev/null
 echo "====================================="
 echo "Logging the user to medperf"
 echo "====================================="
-echo -e "testdataowner\ntest\n" | medperf --ui STDIN login
+echo -e "${ADMIN_USERNAME}\n${ADMIN_PASS}\n" | medperf --ui STDIN --host=$SERVER_URL login
 if [ "$?" -ne "0" ]; then
   echo "Login failed"
   exit 1
@@ -22,7 +38,7 @@ echo "\n"
 echo "====================================="
 echo "Running data preparation step"
 echo "====================================="
-echo -e "Y\nname\ndescription\nlocation\nY\n" | medperf prepare -b 1 -d /tmp/mock_chexpert -l /tmp/mock_chexpert/valid.csv
+echo -e "Y\nname\ndescription\nlocation\nY\n" | medperf --host=$SERVER_URL prepare -b 1 -d /tmp/mock_chexpert -l /tmp/mock_chexpert/valid.csv
 if [ "$?" -ne "0" ]; then
   echo "Data preparation step failed"
   exit 2
@@ -32,7 +48,7 @@ DSET_UID=$(medperf datasets | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2)
 echo "====================================="
 echo "Running benchmark execution step"
 echo "====================================="
-echo -e "Y\n" | medperf execute -b 1 -d $DSET_UID -m 2
+echo -e "Y\n" | medperf --host=$SERVER_URL execute -b 1 -d $DSET_UID -m 2
 if [ "$?" -ne "0" ]; then
   echo "Benchmark execution step failed"
   exit 3
