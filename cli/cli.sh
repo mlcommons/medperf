a6c89e6f360d3128ebb94ac6bed5d53627e87483
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-01-11 12:43:45 -0500

    Add download path argument. Change default user

--- cli/cli.sh
+++ cli/cli.sh
@@ -1,35 +1,31 @@
 #! /bin/bash
-while getopts u:p:s: flag
+while getopts u:p:s:d: flag
 do
     case "${flag}" in
-        u) ADMIN_USERNAME=${OPTARG};;
-        p) ADMIN_PASS=${OPTARG};;
+        u) USERNAME=${OPTARG};;
+        p) PASS=${OPTARG};;
         s) SERVER_URL=${OPTARG};;
+        d) DIRECTORY=${OPTARG};;
     esac
 done
-ADMIN_USERNAME="${ADMIN_USERNAME:-admin}"
-ADMIN_PASS="${ADMIN_PASS:-admin}"
+USERNAME="${USERNAME:-testdataowner}"
+PASS="${PASS:-test}"
 SERVER_URL="${SERVER_URL:-http://127.0.0.1:8000}"
+DIRECTORY="${DIRECTORY:-$DIRECTORY}"
 
-echo "Admin username: $ADMIN_USERNAME"
-echo "Admin password: $ADMIN_PASS"
+echo "username: $USERNAME"
+echo "password: $PASS"
 echo "Server URL: $SERVER_URL"
 
 echo "====================================="
-echo "Cleaning up medperf tmp files"
-echo "====================================="
-rm /tmp/mock_chexpert.tar.gz
-rm -fr /tmp/mock_chexpert
-rm -fr ~/.medperf
-echo "====================================="
 echo "Retrieving mock dataset"
 echo "====================================="
-wget -P /tmp https://storage.googleapis.com/medperf-storage/mock_chexpert_dset.tar.gz &> /dev/null
-tar -xzvf /tmp/mock_chexpert_dset.tar.gz -C /tmp &> /dev/null
+wget -P $DIRECTORY https://storage.googleapis.com/medperf-storage/mock_chexpert_dset.tar.gz &> /dev/null
+tar -xzvf $DIRECTORY/mock_chexpert_dset.tar.gz -C $DIRECTORY &> /dev/null
 echo "====================================="
 echo "Logging the user to medperf"
 echo "====================================="
-echo -e "${ADMIN_USERNAME}\n${ADMIN_PASS}\n" | medperf --ui STDIN --host=$SERVER_URL login
+echo -e "${USERNAME}\n${PASS}\n" | medperf --ui STDIN --host=$SERVER_URL login
 if [ "$?" -ne "0" ]; then
   echo "Login failed"
   exit 1
@@ -38,7 +34,7 @@ echo "\n"
 echo "====================================="
 echo "Running data preparation step"
 echo "====================================="
-echo -e "Y\nname\ndescription\nlocation\nY\n" | medperf --host=$SERVER_URL prepare -b 1 -d /tmp/mock_chexpert -l /tmp/mock_chexpert/valid.csv
+echo -e "Y\nname\ndescription\nlocation\nY\n" | medperf --host=$SERVER_URL prepare -b 1 -d $DIRECTORY/mock_chexpert -l $DIRECTORY/mock_chexpert/valid.csv
 if [ "$?" -ne "0" ]; then
   echo "Data preparation step failed"
   exit 2
@@ -56,5 +52,6 @@ fi
 echo "====================================="
 echo "Cleaning up medperf tmp files"
 echo "====================================="
-rm /tmp/mock_chexpert.tar.gz
-rm -fr /tmp/mock_chexpert
+rm $DIRECTORY/mock_chexpert.tar.gz
+rm -fr $DIRECTORY/mock_chexpert
+rm -fr ~/.medperf
