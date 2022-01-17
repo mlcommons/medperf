aa685c5b726dcc5c8fcbbedcee10c2e6c0d4d722
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-01-17 12:48:38 -0500

    Cli commands refactor (#40)
    * CLI Initial commit
    
    * Test benchmark methods
    
    * Test cube get
    
    * Improve benchmark unit testing
    
    * Test reference model in benchmark models
    
    * Test additional models in benchmark
    
    * Implemente cube tests
    
    * Remove hardcoded registration filename
    
    * Remove outdated dset.is_valid
    
    * Disable filesystem functions for testing
    
    * Remove unnecesary remocking
    
    * Implement dataset tests
    
    * Implement registration tests
    
    * add explicit fail to login
    
    * Improve benchmark tests
    
    * Implement result tests
    
    * add utils file to tests
    
    * Implement server interface tests
    
    * move entities tests to their own folder
    
    * Implement utils tests
    
    * Move mocked objects to respective files
    
    * Add versioning to dependencies
    
    * Move commands to respective folder
    
    * fix cube task testing bug
    
    * Remove hardcoded login from associate command
    
    * Add and improve cancelled workflows
    
    * Fix help commands documentation
    
    * Remove unnecessary documentation
    
    * Implement comms and ui interfaces
    
    * Add versioning to dependencies
    
    * Move commands to respective folder
    
    * Remove hardcoded login from associate command
    
    * Add and improve cancelled workflows
    
    * Fix help commands documentation
    
    * Remove unnecessary documentation
    
    * Implement comms and ui interfaces
    
    * Remove unused decorator
    
    * Use underscore convention
    
    * Make Comms an Abstract Class
    
    * Fix authenticate typo
    
    * Add required parameters to UI Interface
    
    * Require UI for comms implementations
    
    * Fix config not imported
    
    * Add init file to medperf
    
    * Add missing method to comms interface
    
    * Make msg optional for print
    
    * Use underscore naming convention
    
    * Make upload_dataset abstract method
    
    * Adapt tests to new design patterns
    
    * Implement rest tests
    
    * Removed unused dependency
    
    * Simplify text setter
    
    * fix hidden input not returning anything
    
    * Implement cli ui tests
    
    * Remove duplicated authenticate method
    
    * remove unnecessary pass statements
    
    * REST follows comms init signature
    
    * Fix Associate command
    
    * Provide more user-friendly error on Dataset init
    
    * Replace exceptions to pretty errors
    
    * Remove UI circular import
    
    * Test new pretty errors
    
    * Fix comment typo
    
    * Implement associate tests
    
    * Move prompt logic to comms login method
    
    * Fix test with login method
    
    * Add missing abstract method hidden prompt
    
    * Implement login command tests
    
    * Fix login method
    
    * Implement additional tests
    
    * split execution into methods
    
    * reorder methods
    
    * Add cleanup after benchmark execution
    
    * fix comms not receiving ui instance
    
    * Fix registration not using ui for prompt
    
    * Refactor prepare command into methods
    
    * Add cleanup to preparation flow
    
    * Store datasets under their generated_uid
    
    * Split dset preparation and registration commands
    
    * Adjust tests to dataset changes
    
    * Initial submit command
    
    * Fix Benchmark Execution not finding results path
    
    * DRY up comms and ui fixtures
    
    * Remove unused deprecated method
    
    * Implement prepare command tests
    
    * start working on datasets command tests
    
    * Test preparation flow
    
    * Implement benchmark execution tests
    
    * Fix results_path not returning anything
    
    * add results filename to config
    
    * Implement missing benchmark exec tests
    
    * Implement register tests
    
    * Move shared function to utils
    
    * Implement submit tests
    
    * Fix execute command data uid as str
    
    * Fix pretty error not receiving ui
    
    * Remove leftover files from merge
    
    * Recover comms and ui fixtures
    
    * Fix broken comms fixture
    
    * Remove deprecated file
    
    * Fix result not using dataset server uid
    
    * Fix ensure storage is setup before anything
    
    * Fix prepare not storing/registering datset
    
    * Fix detection of already prepared datasets
    
    * Add docstring to submit command
    
    * Fix login test not mocking "exist" method
    
    * Fix benchmark_uid typo
    
    * Fix benchmark_uid typo

--- cli/medperf/decorators.py
+++ cli/medperf/decorators.py
@@ -1,10 +1,8 @@
 from typing import Type
 from collections.abc import Callable
 import logging
-import os
 
-from medperf.utils import cleanup, pretty_error
-from medperf.config import config
+from medperf.utils import cleanup
 
 
 def clean_except(func: Callable) -> Callable:
@@ -27,40 +25,5 @@ def clean_except(func: Callable) -> Callable:
             logging.error(e)
             cleanup()
             raise e
-
-    return wrapper
-
-
-def authenticate(func: Callable) -> Callable:
-    """Decorator for exeuting a command as an authenticated user. For this, the
-    decorator initializes and authenticates a server instance with the already
-    saved credentials token in ~/.medperf/credentials
-
-    Decorated command MUST have a parameter named server
-
-    CLI must have been setup before with:
-        medperf login
-
-    Args:
-        func (Callable): command to run as an authenticated user
-
-    Returns:
-        Callable: command wrapped with the authentication layer
-    """
-
-    def wrapper(*args, **kwargs):
-        cred_path = config["credentials_path"]
-        if os.path.exists(cred_path):
-            with open(cred_path) as f:
-                token = f.readline()
-        else:
-            pretty_error(
-                "Couldn't find credentials file. Did you run 'medperf login' before?"
-            )
-
-        server = Server(config["server"], token)
-
-        kwargs["server"] = server
-        return func(*args, **kwargs)
 
     return wrapper
