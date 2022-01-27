bcec45188cf6378999ce97abbd9ed6cdc52c8e3c
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-01-27 15:52:56 -0500

    Fix test. Add new test

--- cli/medperf/tests/commands/dataset/test_list.py
+++ cli/medperf/tests/commands/dataset/test_list.py
@@ -5,16 +5,27 @@ from medperf.comms import Comms
 from medperf.entities import Dataset, Benchmark
 from medperf.commands.dataset.list import DatasetsList
 
-patch_datasets = "medperf.commands.dataset.list.{}"
+PATCH_DATASETS = "medperf.commands.dataset.list.{}"
 
 
-def test_retrieves_all_benchmarks(mocker, ui):
+def test_retrieves_all_local_benchmarks(mocker, comms, ui):
     # Arrange
-    spy = mocker.patch(patch_datasets.format("Dataset.all"), return_value=[])
+    spy = mocker.patch(PATCH_DATASETS.format("Dataset.all"), return_value=[])
 
     # Act
-    DatasetsList.run(ui)
+    DatasetsList.run(comms, ui)
 
     # Assert
     spy.assert_called_once_with(ui)
 
+
+def test_retrieves_all_remote_benchmarks(mocker, comms, ui):
+    # Arrange
+    mocker.patch(PATCH_DATASETS.format("Dataset.all"), return_value=[])
+    spy = mocker.patch.object(comms, "get_user_datasets", return_value=[])
+
+    # Act
+    DatasetsList.run(comms, ui)
+
+    # Assert
+    spy.assert_called_once()
