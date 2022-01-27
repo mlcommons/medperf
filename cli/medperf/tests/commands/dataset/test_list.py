d60b153c9ef25a25004d2112808f27f7caa307f6
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-01-27 13:24:17 -0500

    Move dataset test files

--- cli/medperf/tests/commands/dataset/test_list.py
+++ cli/medperf/tests/commands/dataset/test_list.py
@@ -3,9 +3,9 @@ import pytest
 from medperf.ui import UI
 from medperf.comms import Comms
 from medperf.entities import Dataset, Benchmark
-from medperf.commands.dataset.list import Datasets
+from medperf.commands.dataset.list import DatasetsList
 
-patch_datasets = "medperf.commands.datasets.{}"
+patch_datasets = "medperf.commands.dataset.list.{}"
 
 
 def test_retrieves_all_benchmarks(mocker, ui):
@@ -13,7 +13,7 @@ def test_retrieves_all_benchmarks(mocker, ui):
     spy = mocker.patch(patch_datasets.format("Dataset.all"), return_value=[])
 
     # Act
-    Datasets.run(ui)
+    DatasetsList.run(ui)
 
     # Assert
     spy.assert_called_once_with(ui)
