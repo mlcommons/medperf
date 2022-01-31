d3f1c5edbafe17c678d2ab239b53a9c31e2afd81
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-01-31 18:50:04 -0500

    Retrieve remote results

--- cli/medperf/commands/result/list.py
+++ cli/medperf/commands/result/list.py
@@ -1,16 +1,18 @@
 from tabulate import tabulate
 
 from medperf.ui import UI
+from medperf.comms import Comms
 from medperf.entities import Result
 
 
 class ResultsList:
     @staticmethod
-    def run(ui: UI):
+    def run(comms: Comms, ui: UI):
         """Lists all local datasets
 	    """
         results = Result.all(ui)
-        headers = ["Benchmark UID", "Model UID", "Data UID", "Submitted"]
+        headers = ["Benchmark UID", "Model UID", "Data UID", "Submitted", "Local"]
+        # Get local results data
         results_data = [
             [
                 result.benchmark_uid,
@@ -19,6 +21,13 @@ class ResultsList:
                 result.uid is not None,
             ]
             for result in results
+        ]
+
+        # Get remote results data
+        remote_results = comms.get_user_results()
+        remote_data = [
+            [result["benchmark"], result["model"], result["dataset"],]
+            for result in remote_results
         ]
         tab = tabulate(results_data, headers=headers)
         ui.print(tab)
