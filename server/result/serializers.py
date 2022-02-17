d70b995ae7daea5f475ffcf92649ac81e82dad20
Johnu George <johnugeorge109@gmail.com>
2022-02-17 15:14:23 +0530

    Added validation for Result submission

--- server/result/serializers.py
+++ server/result/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from benchmarkdataset.models import BenchmarkDataset
+from benchmarkmodel.models import BenchmarkModel
+
 from .models import ModelResult
 
 
@@ -7,3 +10,33 @@ class ModelResultSerializer(serializers.ModelSerializer):
         model = ModelResult
         fields = "__all__"
         read_only_fields = ["owner", "approved_at", "approval_status"]
+
+    def validate(self, data):
+        benchmark = data["benchmark"]
+        mlcube = data["model"]
+        dataset = data["dataset"]
+        if data["benchmark"].state != "OPERATION":
+            raise serializers.ValidationError("Benchmark state must be in operation")
+        if data["model"].state != "OPERATION":
+            raise serializers.ValidationError("MlCube state must be in operation")
+        if data["dataset"].state != "OPERATION":
+            raise serializers.ValidationError("Dataset state must be in operation")
+        last_benchmarkmodel = (
+            BenchmarkModel.objects.filter(benchmark__id=benchmark.id, model_mlcube__id=mlcube.id)
+            .order_by("-created_at")
+            .first()
+        )
+        if not last_benchmarkmodel:
+            raise serializers.ValidationError("Mlcube must be associated to the benchmark")
+        else:
+            if last_benchmarkmodel.approval_status != "APPROVED":
+                raise serializers.ValidationError("Mlcube-Benchmark association must be approved")
+        last_benchmarkdataset = (
+            BenchmarkDataset.objects.filter(benchmark__id=benchmark.id, dataset__id=dataset.id).order_by("-created_at").first()
+        )
+        if not last_benchmarkdataset:
+            raise serializers.ValidationError("Dataset must be associated to the benchmark")
+        else:
+            if last_benchmarkdataset.approval_status != "APPROVED":
+                raise serializers.ValidationError("Dataset-Benchmark association must be approved")
+        return data
