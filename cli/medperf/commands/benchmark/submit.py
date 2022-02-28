e400317fdf72059a32fa40d34c9ee2acae1bdf67
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-02-28 12:57:08 -0500

    restructure is_valid

--- cli/medperf/commands/benchmark/submit.py
+++ cli/medperf/commands/benchmark/submit.py
@@ -28,6 +28,7 @@ class SubmitBenchmark:
         self.name = None
         self.description = None
         self.docs_url = None
+        self.demo_url = None
         self.data_preparation_mlcube = None
         self.reference_model_mlcube = None
         self.data_evaluator_mlcube = None
@@ -36,17 +37,26 @@ class SubmitBenchmark:
         name_prompt = "Name: "
         desc_prompt = "Description: "
         docs_url_prompt = "Documentation URL: "
+        demo_dset_prompt = "Demo Dataset Tarball URL: "
         prep_uid_prompt = "Data Preparation MLCube UID: "
         model_uid_prompt = "Reference Model MLCube UID: "
         eval_uid_prompt = "Data Evaluator MLCube UID: "
         self.__get_or_print("name", name_prompt)
         self.__get_or_print("description", desc_prompt)
         self.__get_or_print("docs_url", docs_url_prompt)
+        self.__get_or_print("demo_url", demo_dset_prompt)
         self.__get_or_print("data_preparation_mlcube", prep_uid_prompt)
         self.__get_or_print("reference_model_mlcube", model_uid_prompt)
         self.__get_or_print("data_evaluator_mlcube", eval_uid_prompt)
 
-    def __get_or_print(self, attr, prompt):
+    def __get_or_print(self, attr: str, prompt: str):
+        """Will print the value stored in the provided attribute. If value is
+        None, it will prompt the user with the given prompt.
+
+        Args:
+            attr (str): attribute to check and/or write to.
+            prompt (str): message to display to the user if value is None.
+        """
         attr_val = getattr(self, attr)
         if attr_val is None or attr_val == "":
             setattr(self, attr, self.ui.prompt(prompt))
@@ -62,35 +72,39 @@ class SubmitBenchmark:
         name_valid_length = 0 < len(self.name) < 20
         desc_valid_length = len(self.description) < 100
         docs_url_valid = self.docs_url == "" or validators.url(self.docs_url)
+        demo_url_valid = self.demo_url == "" or validators.url(self.demo_url)
         prep_uid_valid = self.data_preparation_mlcube.isdigit()
         model_uid_valid = self.reference_model_mlcube.isdigit()
         eval_uid_valid = self.data_evaluator_mlcube.isdigit()
 
+        valid_tests = [
+            ("name", name_valid_length, "Name is invalid"),
+            ("description", desc_valid_length, "Description is too long"),
+            ("docs_url", docs_url_valid, "Documentation URL is invalid"),
+            ("demo_url", demo_url_valid, "Demo Dataset Tarball URL is invalid"),
+            (
+                "data_preparation_mlcube",
+                prep_uid_valid,
+                "Data Preparation MLCube UID is invalid",
+            ),
+            (
+                "reference_model_mlcube",
+                model_uid_valid,
+                "Reference Model MLCube is invalid",
+            ),
+            (
+                "data_evaluator_mlcube",
+                eval_uid_valid,
+                "Data Evaluator MLCube is invalid",
+            ),
+        ]
+
         valid = True
-        if not name_valid_length:
-            valid = False
-            self.name = None
-            self.ui.print_error("Name is invalid")
-        if not desc_valid_length:
-            valid = False
-            self.description = None
-            self.ui.print_error("Description is too long")
-        if not docs_url_valid:
-            valid = False
-            self.docs_url = None
-            self.ui.print_error("Documentation URL is invalid")
-        if not prep_uid_valid:
-            valid = False
-            self.data_preparation_mlcube = None
-            self.ui.print_error("Data Preparation MLCube UID is invalid")
-        if not model_uid_valid:
-            valid = False
-            self.reference_model_mlcube = None
-            self.ui.print_error("Reference Model MLCube is invalid")
-        if not eval_uid_valid:
-            valid = False
-            self.data_evaluator_mlcube = None
-            self.ui.print_error("Data Evaluator MLCube is invalid")
+        for attr, test, error_msg in valid_tests:
+            if not test:
+                valid = False
+                setattr(self, attr, None)
+                ui.print_error(error_msg)
 
         return valid
 
@@ -99,12 +113,13 @@ class SubmitBenchmark:
             "name": self.name,
             "description": self.description,
             "docs_url": self.docs_url,
+            "demo_dataset_tarball_url": self.demo_url,
             "data_preparation_mlcube": int(self.data_preparation_mlcube),
             "reference_model_mlcube": int(self.reference_model_mlcube),
             "data_evaluator_mlcube": int(self.data_evaluator_mlcube),
             "state": "OPERATION",
             "is_valid": True,
-            "approval_status": "APPROVED",
+            "approval_status": "PENDING",
         }
 
     def submit(self):
