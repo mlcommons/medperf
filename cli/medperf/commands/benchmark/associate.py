71b1c397237d8bf9e72a2f49afd2e089f75e78e1
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-02-28 18:35:00 -0500

    Benchmark Association initial implementation

---
+++ cli/medperf/commands/benchmark/associate.py
@@ -0,0 +1,30 @@
+from medperf.ui import UI
+from medperf.comms import Comms
+from medperf.utils import pretty_error
+
+
+class AssociateBenchmark:
+    @classmethod
+    def run(
+        cls, benchmark_uid: str, model_uid: str, dset_uid: str, comms: Comms, ui: UI
+    ):
+        """Associates a dataset or model to the given benchmark
+
+        Args:
+            benchmark_uid (str): UID of benchmark to associate entities with
+            model_uid (str): UID of model to associate with benchmark
+            dset_uid (str): UID of dataset to associate with benchmark
+            comms (Comms): _description_
+            ui (UI): _description_
+        """
+        if model_uid and dset_uid:
+            pretty_error(
+                "Can only associate one entity at a time. Pass a model or a dataset only",
+                ui,
+            )
+        if model_uid is not None:
+            # TODO: changes from mlcube subcommands are required
+            pass
+        if dset_uid is not None:
+            comms.associate_dset_benchmark(dset_uid, benchmark_uid)
+
