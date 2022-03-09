85dcabd7a2803abcf3534d7541a377774578739f
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-03-09 17:11:57 -0500

    Fix associations

--- cli/medperf/commands/associations.py
+++ cli/medperf/commands/associations.py
@@ -8,27 +8,21 @@ class Associations:
     @staticmethod
     def run(comms: Comms, ui: UI, filter: str = None):
         """Get Pending association requests"""
-        filter = filter.upper()
         dset_assocs = comms.get_dataset_associations()
         cube_assocs = comms.get_cube_associations()
 
         # Might be worth seeing if creating an association class that encapsulates
         # most of the logic here is useful
-        if filter:
-            dset_assocs = [
-                assoc for assoc in dset_assocs if assoc["approval_status"] == filter
-            ]
-            cube_assocs = [
-                assoc for assoc in cube_assocs if assoc["approval_status"] == filter
-            ]
-
         assocs = dset_assocs + cube_assocs
+        if filter:
+            filter = filter.upper()
+            assocs = [assoc for assoc in assocs if assoc["approval_status"] == filter]
 
         assocs_info = []
         for assoc in assocs:
             assoc_info = (
-                assoc.get("dataset", "-"),
-                assoc.get("model_mlcube", "-"),
+                assoc.get("dataset", None),
+                assoc.get("model_mlcube", None),
                 assoc["benchmark"],
                 assoc["initiated_by"],
                 assoc["approval_status"],
