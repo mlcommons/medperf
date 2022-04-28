3188bd61170106aef960f73d0edf0de76292b5b5
Johnu George <johnugeorge109@gmail.com>
2022-04-29 00:04:25 +0530

    Exclude migrations folder

--- server/dataset/migrations/0001_initial.py
+++ server/dataset/migrations/0001_initial.py
@@ -10,67 +10,32 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("mlcube", "0001_initial"),
+        ('mlcube', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name="Dataset",
+            name='Dataset',
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("name", models.CharField(max_length=20)),
-                ("description", models.CharField(blank=True, max_length=20)),
-                ("location", models.CharField(blank=True, max_length=100)),
-                ("input_data_hash", models.CharField(max_length=128)),
-                ("generated_uid", models.CharField(max_length=128, unique=True)),
-                ("split_seed", models.IntegerField()),
-                ("is_valid", models.BooleanField(default=True)),
-                (
-                    "state",
-                    models.CharField(
-                        choices=[
-                            ("DEVELOPMENT", "DEVELOPMENT"),
-                            ("OPERATION", "OPERATION"),
-                        ],
-                        default="DEVELOPMENT",
-                        max_length=100,
-                    ),
-                ),
-                (
-                    "generated_metadata",
-                    models.JSONField(blank=True, default=dict, null=True),
-                ),
-                (
-                    "user_metadata",
-                    models.JSONField(blank=True, default=dict, null=True),
-                ),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                ("modified_at", models.DateTimeField(auto_now=True)),
-                (
-                    "data_preparation_mlcube",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.PROTECT,
-                        related_name="benchmark_preprocessor_mlcube",
-                        to="mlcube.mlcube",
-                    ),
-                ),
-                (
-                    "owner",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.PROTECT,
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=20)),
+                ('description', models.CharField(blank=True, max_length=20)),
+                ('location', models.CharField(blank=True, max_length=100)),
+                ('input_data_hash', models.CharField(max_length=128)),
+                ('generated_uid', models.CharField(max_length=128, unique=True)),
+                ('split_seed', models.IntegerField()),
+                ('is_valid', models.BooleanField(default=True)),
+                ('state', models.CharField(choices=[('DEVELOPMENT', 'DEVELOPMENT'), ('OPERATION', 'OPERATION')], default='DEVELOPMENT', max_length=100)),
+                ('generated_metadata', models.JSONField(blank=True, default=dict, null=True)),
+                ('user_metadata', models.JSONField(blank=True, default=dict, null=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+                ('data_preparation_mlcube', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='benchmark_preprocessor_mlcube', to='mlcube.mlcube')),
+                ('owner', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
             ],
-            options={"ordering": ["modified_at"],},
+            options={
+                'ordering': ['modified_at'],
+            },
         ),
     ]
