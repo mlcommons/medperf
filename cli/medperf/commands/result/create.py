5af56474b1e60f994b5dc28c12671fd0a5247526
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-05-17 11:08:53 -0500

    Remove TODO

--- cli/medperf/commands/result/create.py
+++ cli/medperf/commands/result/create.py
@@ -81,7 +81,6 @@ class BenchmarkExecution:
         preds_path = os.path.join(config.predictions_storage, model_uid, data_uid)
         preds_path = storage_path(preds_path)
         data_path = self.dataset.data_path
-        TODO # make sure evaluations are done with the labels path if the dataset contains separate labels path
         self.model_cube.run(
             self.ui, task="infer", data_path=data_path, output_path=preds_path
         )
