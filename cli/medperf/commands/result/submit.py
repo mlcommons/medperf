6d6f75cc7f7452b08a094f729687c2ac8101a082
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-05-24 10:21:47 -0500

    Add approval argument to result submission

--- cli/medperf/commands/result/submit.py
+++ cli/medperf/commands/result/submit.py
@@ -4,22 +4,24 @@ from medperf.entities.dataset import Dataset
 
 
 class ResultSubmission:
-    def __init__(self, benchmark_uid, data_uid, model_uid, comms, ui):
+    def __init__(self, benchmark_uid, data_uid, model_uid, comms, ui, approved=False):
         self.benchmark_uid = benchmark_uid
         self.data_uid = data_uid
         self.model_uid = model_uid
         self.comms = comms
         self.ui = ui
+        self.approved = approved
 
     @classmethod
-    def run(cls, benchmark_uid, data_uid, model_uid, comms, ui):
+    def run(cls, benchmark_uid, data_uid, model_uid, comms, ui, approved=False):
         dset = Dataset(data_uid, ui)
-        sub = cls(benchmark_uid, dset.uid, model_uid, comms, ui)
+        sub = cls(benchmark_uid, dset.uid, model_uid, comms, ui, approved=approved)
         sub.upload_results()
 
     def upload_results(self):
         result = Result(self.benchmark_uid, self.data_uid, self.model_uid)
-        approved = result.request_approval(self.ui)
+        approved = self.approved or result.request_approval(self.ui)
+
         if not approved:
             msg = "Results upload operation cancelled"
             pretty_error(msg, self.ui, add_instructions=False)
