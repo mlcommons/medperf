ffe7ef873428bfa163c672939e1c885e8615235d
hasan7n <78664424+hasan7n@users.noreply.github.com>
2022-07-28 18:16:56 +0200

    fix benchmark associate with no entities (#215)

--- cli/medperf/tests/commands/benchmark/test_associate.py
+++ cli/medperf/tests/commands/benchmark/test_associate.py
@@ -10,6 +10,7 @@ PATCH_ASSOC = "medperf.commands.benchmark.associate.{}"
 @pytest.mark.parametrize("data_uid", [None, "1"])
 def test_run_fails_if_model_and_dset_passed(mocker, model_uid, data_uid, comms, ui):
     # Arrange
+    num_arguments = int(data_uid is None) + int(model_uid is None)
     spy = mocker.patch(PATCH_ASSOC.format("pretty_error"))
     mocker.patch.object(comms, "associate_cube")
     mocker.patch.object(comms, "associate_dset")
@@ -20,7 +21,7 @@ def test_run_fails_if_model_and_dset_passed(mocker, model_uid, data_uid, comms, 
     AssociateBenchmark.run("1", model_uid, data_uid, comms, ui)
 
     # Assert
-    if model_uid and data_uid:
+    if num_arguments != 1:
         spy.assert_called_once()
     else:
         spy.assert_not_called()
