edd61b92693902d23d96373138be5dafbd76ca67
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-08-09 14:00:00 -0500

    Entity interface initial implementation

---
+++ cli/medperf/entities/entity.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+from abc import ABC, abstractmethod
+
+from medperf.ui.interface import UI
+
+class Entity:
+	@abstractmethod
+	def all(cls) -> List["Entity"]:
+		"""Gets a list of all instances of the respective entity. 
+		Wether the list is local or remote depends on the implementation.
+
+		Returns:
+			List[Entity]: a list of entities.
+		"""
+
+	@abstractmethod
+	def get(cls, uid: str) -> "Entity":
+		"""Gets an instance of the respective entity.
+		Wether this requires only local read or remote calls depends 
+		on the implementation.
+
+		Args:
+			uid (str): Unique Identifier to retrieve the entity
+
+		Returns:
+			Entity: Entity Instance associated to the UID
+		"""
+
+	@abstractmethod
+	def todict(self) -> Dict:
+		"""Dictionary representation of the entity
+
+		Returns:
+			Dict: Dictionary containing information about the entity
+		"""
