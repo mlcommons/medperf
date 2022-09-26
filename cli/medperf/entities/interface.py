f0c4756eb46dd09520cb60064a63cc4475bd9892
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-09-26 12:07:20 -0500

    Fix indentation

--- cli/medperf/entities/interface.py
+++ cli/medperf/entities/interface.py
@@ -9,34 +9,34 @@ class Entity:
     @abstractmethod
     def all(cls) -> List["Entity"]:
         """Gets a list of all instances of the respective entity. 
-		Wether the list is local or remote depends on the implementation.
+        Wether the list is local or remote depends on the implementation.
 
-		Returns:
-			List[Entity]: a list of entities.
-		"""
+        Returns:
+            List[Entity]: a list of entities.
+        """
 
     @abstractmethod
     def get(cls, uid: str) -> "Entity":
         """Gets an instance of the respective entity.
-		Wether this requires only local read or remote calls depends 
-		on the implementation.
+        Wether this requires only local read or remote calls depends 
+        on the implementation.
 
-		Args:
-			uid (str): Unique Identifier to retrieve the entity
+        Args:
+            uid (str): Unique Identifier to retrieve the entity
 
-		Returns:
-			Entity: Entity Instance associated to the UID
-		"""
+        Returns:
+            Entity: Entity Instance associated to the UID
+        """
 
     @abstractmethod
     def todict(self) -> Dict:
         """Dictionary representation of the entity
 
-		Returns:
-			Dict: Dictionary containing information about the entity
-		"""
+        Returns:
+            Dict: Dictionary containing information about the entity
+        """
 
     @abstractmethod
     def upload(self, comms: Comms):
         """Upload the entity-related information to the communication's interface
-		"""
+        """
