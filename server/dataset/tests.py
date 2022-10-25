6a7bd53225e235271e74aa47b9b683e191829454
hasan7n <hasankassim7@hotmail.com>
2022-10-25 02:23:12 +0200

    apply pagination to all list apis

--- server/dataset/tests.py
+++ server/dataset/tests.py
@@ -76,7 +76,7 @@ class DatasetTest(MedPerfTest):
 
         response = self.client.get("/datasets/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data["results"]), 1)
 
         newtestdataset = {
             "name": "newdataset",
