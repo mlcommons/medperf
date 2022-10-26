259fa923b43235528d43babe628cffa80391cce0
cassianomoraes <cassiano@writechoice.com.br>
2022-10-26 12:09:32 -0300

    Update gen_ref_pages.py

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -9,7 +9,7 @@ import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
 build_params = [
-    ("cli/medperf", "cli/medperf", "cli/medperf", "reference"),
+    ("cli, "cli/medperf", "cli", "reference"),
     # ("server", "server", "", "reference"),
 ]
 
