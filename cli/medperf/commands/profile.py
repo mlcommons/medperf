c87db3a1cc17dec744b096effde5794058edae4b
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-11-07 10:04:16 -0500

    Convert tabs to spaces

--- cli/medperf/commands/profile.py
+++ cli/medperf/commands/profile.py
@@ -9,102 +9,102 @@ from medperf.utils import parse_context_args, dict_pretty_print, pretty_error
 app = typer.Typer()
 
 def read_config():
-	config_p = configparser.ConfigParser()
-	config_path = os.path.join(config.storage, config.config_path)
-	config_p.read(config_path)
-	return config_p
+    config_p = configparser.ConfigParser()
+    config_path = os.path.join(config.storage, config.config_path)
+    config_p.read(config_path)
+    return config_p
 
 def write_config(config_p: configparser.ConfigParser):
-	config_path = os.path.join(config.storage, config.config_path)
-	with open(config_path, "w") as f:
-		config_p.write(f)
+    config_path = os.path.join(config.storage, config.config_path)
+    with open(config_path, "w") as f:
+        config_p.write(f)
 
 @app.command("create", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 @docstring_parameter(" | ".join(config.customizable_params))
 def create(
-	ctx: typer.Context,
-	name: str = typer.Option(..., "--name", "-n", help="Profile's name"),
+    ctx: typer.Context,
+    name: str = typer.Option(..., "--name", "-n", help="Profile's name"),
 ):
-	"""Creates a new profile for managing and customizing configuration
+    """Creates a new profile for managing and customizing configuration
 
-	Extra arguments in the format "--key=value" will be handled as custom configuration
-	parameters that will be stored under the new profile
+    Extra arguments in the format "--key=value" will be handled as custom configuration
+    parameters that will be stored under the new profile
 
-	Arguments:
+    Arguments:
 
-	{0}
-	"""
-	args = parse_context_args(ctx.args)
-	config_p = read_config()
+    {0}
+    """
+    args = parse_context_args(ctx.args)
+    config_p = read_config()
 
-	filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
-	if name in config_p:
-		pretty_error("A profile with the same name already exists", config.ui)
+    filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
+    if name in config_p:
+        pretty_error("A profile with the same name already exists", config.ui)
 
-	config_p[name] = filtered_params
-	write_config(config_p)
+    config_p[name] = filtered_params
+    write_config(config_p)
 
 @app.command("set", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 @docstring_parameter(" | ".join(config.customizable_params))
 def set(ctx: typer.Context):
-	"""Assign key-value configuration pairs to the current profile.
+    """Assign key-value configuration pairs to the current profile.
 
-	Extra arguments in the format "--key=value" will be handled as custom configuration
+    Extra arguments in the format "--key=value" will be handled as custom configuration
 
-	Available arguments:
+    Available arguments:
 
-	{0}
-	"""
-	profile = config.profile
-	args = parse_context_args(ctx.args)
-	config_p = read_config()
+    {0}
+    """
+    profile = config.profile
+    args = parse_context_args(ctx.args)
+    config_p = read_config()
 
-	filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
-	current_config = config_p[profile]
-	current_config.update(filtered_params)
-	config_p[profile] = current_config
-	write_config(config_p)
+    filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
+    current_config = config_p[profile]
+    current_config.update(filtered_params)
+    config_p[profile] = current_config
+    write_config(config_p)
 
 @app.command("unset", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 @docstring_parameter(" | ".join(config.customizable_params))
 def unset(ctx: typer.Context):
-	"""Removes a set of custom configuration parameters assigned to the current profile.
+    """Removes a set of custom configuration parameters assigned to the current profile.
 
-	A list of space-separated keys is expected
+    A list of space-separated keys is expected
 
-	Available arguments:
+    Available arguments:
 
-	{0}
-	"""
-	profile = config.profile
-	args = ctx.args
-	config_p = read_config()
+    {0}
+    """
+    profile = config.profile
+    args = ctx.args
+    config_p = read_config()
 
-	for key in args:
-		if key in config.customizable_params:
-			del config_p[profile][key]
-	write_config(config_p)
+    for key in args:
+        if key in config.customizable_params:
+            del config_p[profile][key]
+    write_config(config_p)
 
 @app.command("ls")
 def list():
-	"""Lists all available profiles
-	"""
-	ui = config.ui
-	config_p = read_config()
-	for profile in config_p:
-		ui.print(profile)
+    """Lists all available profiles
+    """
+    ui = config.ui
+    config_p = read_config()
+    for profile in config_p:
+        ui.print(profile)
 
 @app.command("view")
 def view(profile: str = typer.Argument(None)):
-	"""Displays a profile's configuration.
+    """Displays a profile's configuration.
 
-	Args:
-		profile (str, optional): Profile to display information from. Defaults to active profile.
-	"""
-	ui = config.ui
-	if profile is None:
-		profile = config.profile
+    Args:
+        profile (str, optional): Profile to display information from. Defaults to active profile.
+    """
+    ui = config.ui
+    if profile is None:
+        profile = config.profile
 
-	config_p = read_config()
-	profile_config = config_p[profile]
-	dict_pretty_print(profile_config, ui)
+    config_p = read_config()
+    profile_config = config_p[profile]
+    dict_pretty_print(profile_config, ui)
