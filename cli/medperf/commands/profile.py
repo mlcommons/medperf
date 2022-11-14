90d1f108cadd3e5dcf6ae08cc32ff8994b3850aa
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-11-14 11:36:05 -0500

    Print error if invalid arguments

--- cli/medperf/commands/profile.py
+++ cli/medperf/commands/profile.py
@@ -22,6 +22,18 @@ def write_config(config_p: configparser.ConfigParser):
         config_p.write(f)
 
 
+def validate_custom_args(args: dict):
+    """Checks that the passed arguments are all part of the customizable arguments,
+    prints an error otherwise
+
+    Args:
+        args (dict): parsed cli arguments
+    """
+    invalid_args = set(args.keys()) - set(config.customizable_params)
+    if len(invalid_args):
+        pretty_error(f"Invalid arguments passed: {', '.join(invalid_args)}", add_instructions=False)
+
+
 @app.command("create", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 @docstring_parameter(" | ".join(config.customizable_params))
 def create(
@@ -30,7 +42,7 @@ def create(
 ):
     """Creates a new profile for managing and customizing configuration
 
-    Extra arguments in the format "--key=value" will be handled as custom configuration
+    Arguments in the format "--key=value" will be handled as custom configuration
     parameters that will be stored under the new profile
 
     Arguments:
@@ -40,20 +52,20 @@ def create(
     args = parse_context_args(ctx.args)
     config_p = read_config()
 
-    filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
+    validate_custom_args(args)
     if name in config_p:
-        pretty_error("A profile with the same name already exists", config.ui)
+        pretty_error("A profile with the same name already exists")
 
-    config_p[name] = filtered_params
+    config_p[name] = args
     write_config(config_p)
 
 
 @app.command("set", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 @docstring_parameter(" | ".join(config.customizable_params))
-def set(ctx: typer.Context):
+def set_args(ctx: typer.Context):
     """Assign key-value configuration pairs to the current profile.
 
-    Extra arguments in the format "--key=value" will be handled as custom configuration
+    Arguments in the format "--key=value" will be handled as custom configuration
 
     Available arguments:
 
@@ -63,9 +75,9 @@ def set(ctx: typer.Context):
     args = parse_context_args(ctx.args)
     config_p = read_config()
 
-    filtered_params = {k:v for k, v in args.items() if k in config.customizable_params}
+    validate_custom_args(args)
     current_config = config_p[profile]
-    current_config.update(filtered_params)
+    current_config.update(args)
     config_p[profile] = current_config
     write_config(config_p)
 
@@ -108,10 +120,9 @@ def view(profile: str = typer.Argument(None)):
     Args:
         profile (str, optional): Profile to display information from. Defaults to active profile.
     """
-    ui = config.ui
     if profile is None:
         profile = config.profile
 
     config_p = read_config()
     profile_config = config_p[profile]
-    dict_pretty_print(profile_config, ui)
+    dict_pretty_print(profile_config)
