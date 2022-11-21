1747bd419ea46ed2dda86c268ee4dc73a26ee139
hasan7n <hasankassim7@hotmail.com>
2022-11-21 02:04:25 +0100

    create serializer and view for modifying priority

--- server/benchmarkmodel/views.py
+++ server/benchmarkmodel/views.py
@@ -8,6 +8,7 @@ from .permissions import IsAdmin, IsMlCubeOwner, IsBenchmarkOwner
 from .serializers import (
     BenchmarkModelListSerializer,
     ModelApprovalSerializer,
+    ModelPrioritySerializer,
 )
 
 
@@ -90,3 +91,29 @@ class ModelApproval(GenericAPIView):
         benchmarkmodel = self.get_object(pk, bid)
         benchmarkmodel.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ModelPriority(GenericAPIView):
+    permission_classes = [IsAdmin | IsBenchmarkOwner]
+    serializer_class = ModelPrioritySerializer
+    queryset = ""
+
+    def get_object(self, model_id, benchmark_id):
+        try:
+            return BenchmarkModel.objects.filter(
+                model_mlcube__id=model_id, benchmark__id=benchmark_id
+            )
+        except BenchmarkModel.DoesNotExist:
+            raise Http404
+
+    def put(self, request, pk, bid, format=None):
+        """
+        Update priority of the last benchmark model association
+        """
+        benchmarkmodel = self.get_object(pk, bid).order_by("-created_at").first()
+        serializer = ModelPrioritySerializer(benchmarkmodel, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # TODO: return a the new ordered models list
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
