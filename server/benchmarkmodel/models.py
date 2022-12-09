7e2f39cbd95b7f2ef5be6c44d978c1b9788d9b40
hasan7n <hasankassim7@hotmail.com>
2022-12-09 02:09:49 +0100

    make priority field required

--- server/benchmarkmodel/models.py
+++ server/benchmarkmodel/models.py
@@ -18,7 +18,7 @@ class BenchmarkModel(models.Model):
     approved_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    priority = models.FloatField(blank=True, null=True)
+    priority = models.FloatField()
 
     class Meta:
         ordering = ["priority"]
