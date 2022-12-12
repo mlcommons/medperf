60b910f01600a1eb0a4423149f3b0df51409c322
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2022-12-12 16:07:40 -0500

    Add mlcube mocking

--- cli/medperf/tests/entities/test_entity.py
+++ cli/medperf/tests/entities/test_entity.py
@@ -3,7 +3,7 @@ from medperf.entities.benchmark import Benchmark
 from medperf.entities.cube import Cube
 from medperf.entities.dataset import Dataset
 from medperf.entities.result import Result
-from .utils import *
+from medperf.tests.entities.utils import *
 
 
 @pytest.fixture(params=[Benchmark, Cube, Dataset, Result])
@@ -11,27 +11,30 @@ def Implementation(request):
     return request.param
 
 
-@pytest.fixture()
-def setup(Implementation, fs):
+@pytest.fixture(params=[1, 2, 3])
+def setup(request, Implementation, fs):
+    ids = request.param
     if Implementation == Benchmark:
-        setup_benchmark_fs([1, 2, 3], fs)
+        setup_benchmark_fs(ids, fs)
     elif Implementation == Cube:
-        setup_cube_fs([1, 2, 3], fs)
+        setup_cube_fs(ids, fs)
     elif Implementation == Dataset:
-        setup_dset_fs([1, 2, 3], fs)
+        setup_dset_fs(ids, fs)
     elif Implementation == Result:
-        setup_result_fs([1, 2, 3], fs)
+        setup_result_fs(ids, fs)
+    return ids
 
 
+@pytest.mark.parametrize("setup", [[283, 17, 493]], indirect=True)
 def test_all_returns_list_of_entity_instances(Implementation, setup, fs):
     # Arrange
+    ids = setup
 
     # Act
-    # TODO: Get the entity we want to test and call the function
-    # TODO: Entity is a parameter, and the input/output are passed as parameters as well
     entities = Implementation.all()
 
     # Assert
-    # Ensure the output of the call is the expected one
-    assert len(entities) == 3
+    retrieved_ids = [int(e.todict()["id"]) for e in entities]
+    assert type(entities[0]) == Implementation
+    assert ids == retrieved_ids
 
--- cli/medperf/tests/entities/utils.py
+++ cli/medperf/tests/entities/utils.py
@@ -1,6 +1,42 @@
 import os
 from medperf.utils import storage_path
 from medperf import config
+import yaml
+
+from medperf.enums import Status
+
+
+# BENCHMARK MOCKING
+def generate_benchmark(**kwargs):
+    return {
+        "id": kwargs.get("id", 1),
+        "name": kwargs.get("name", "name"),
+        "description": kwargs.get("description", "description"),
+        "docs_url": kwargs.get("docs_url", ""),
+        "created_at": kwargs.get("created_at", "created_at"),
+        "modified_at": kwargs.get("modified_at", "modified_at"),
+        "approved_at": kwargs.get("approved_at", "approved_at"),
+        "owner": kwargs.get("owner", 1),
+        "demo_dataset_tarball_url": kwargs.get(
+            "demo_dataset_tarball_url", "demo_dataset_tarball_url"
+        ),
+        "demo_dataset_tarball_hash": kwargs.get(
+            "demo_dataset_tarball_hash", "demo_dataset_tarball_hash"
+        ),
+        "demo_dataset_generated_uid": kwargs.get(
+            "demo_dataset_generated_uid", "demo_dataset_generated_uid"
+        ),
+        "data_preparation_mlcube": kwargs.get("data_preparation_mlcube", 1),
+        "reference_model_mlcube": kwargs.get("data_preparation_mlcube", 2),
+        "models": kwargs.get("models", [2]),
+        "data_evaluator_mlcube": kwargs.get("data_evaluator_mlcube", 3),
+        "state": kwargs.get("state", "PRODUCTION"),
+        "is_valid": kwargs.get("is_valid", True),
+        "is_active": kwargs.get("is_active", True),
+        "approval_status": kwargs.get("approval_status", Status.PENDING.value),
+        "metadata": kwargs.get("metadata", {}),
+        "user_metadata": kwargs.get("user_metadata", {}),
+    }
 
 
 def setup_benchmark_fs(ids, fs):
@@ -8,7 +44,47 @@ def setup_benchmark_fs(ids, fs):
     for id in ids:
         id = str(id)
         bmk_filepath = os.path.join(bmks_path, id, config.benchmarks_filename)
-        fs.create_file(bmk_filepath)
+        bmk_contents = generate_benchmark(id=id)
+        fs.create_file(bmk_filepath, contents=yaml.dump(bmk_contents))
+
+
+# MLCUBE MOCKING
+def generate_cube(**kwargs):
+    return (
+        {
+            "id": kwargs.get("id", 1),
+            "name": kwargs.get("name", "name"),
+            "git_mlcube_url": kwargs.get("git_mlcube_url", "git_mlcube_url"),
+            "git_parameters_url": kwargs.get(
+                "git_parameters_url", "git_parameters_url"
+            ),
+            "image_tarball_url": kwargs.get("image_tarball_url", "image_tarball_url"),
+            "image_tarball_hash": kwargs.get(
+                "image_tarball_hash", "image_tarball_hash"
+            ),
+            "additional_files_tarball_url": kwargs.get(
+                "additional_files_tarball_url", "additional_files_tarball_url"
+            ),
+            "additional_files_tarball_hash": kwargs.get(
+                "additional_files_tarball_hash", "additional_files_tarball_hash"
+            ),
+            "state": kwargs.get("state", "PRODUCTION"),
+            "is_valid": kwargs.get("is_valid", True),
+            "owner": kwargs.get("owner", 1),
+            "metadata": kwargs.get("metadata", {}),
+            "user_metadata": kwargs.get("user_metadata", {}),
+            "created_at": kwargs.get("created_at", "created_at"),
+            "modified_at": kwargs.get("modified_at", "modified_at"),
+        },
+        {
+            "additional_files_tarball_hash": kwargs.get(
+                "additional_files_tarball_hash", "additional_files_tarball_hash"
+            ),
+            "image_tarball_hash": kwargs.get(
+                "image_tarball_hash", "image_tarball_hash"
+            ),
+        },
+    )
 
 
 def setup_cube_fs(ids, fs):
@@ -17,8 +93,9 @@ def setup_cube_fs(ids, fs):
         id = str(id)
         meta_cube_file = os.path.join(cubes_path, id, config.cube_metadata_filename)
         hash_cube_file = os.path.join(cubes_path, id, config.cube_hashes_filename)
-        fs.create_file(meta_cube_file)
-        fs.create_file(hash_cube_file)
+        meta, hashes = generate_cube(id=id)
+        fs.create_file(meta_cube_file, contents=yaml.dump(meta))
+        fs.create_file(hash_cube_file, contents=yaml.dump(hashes))
 
 
 def setup_dset_fs(ids, fs):
