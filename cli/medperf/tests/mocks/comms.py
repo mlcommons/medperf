385d2390564244f277612966e88c8ca719546531
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-01-04 16:31:54 -0500

    Allow custom entity configuration

--- cli/medperf/tests/mocks/comms.py
+++ cli/medperf/tests/mocks/comms.py
@@ -1,5 +1,5 @@
 # Utility functions for mocking comms and its methods
-from typing import Dict, List, Callable
+from typing import Dict, List, Callable, Union
 from unittest.mock import MagicMock
 from pytest_mock.plugin import MockFixture
 
@@ -11,8 +11,8 @@ def mock_comms_entity_gets(
     comms: MagicMock,
     generate_fn: Callable,
     comms_calls: Dict[str, str],
-    all_ids: List[str],
-    user_ids: List[str],
+    all_ents: List[Union[str, Dict]],
+    user_ents: List[Union[str, Dict]],
     uploaded: List,
 ):
     """Mocks API endpoints used by an entity instance. Allows to define
@@ -28,8 +28,8 @@ def mock_comms_entity_gets(
             - get_user
             - get_instance
             - upload_instance
-        all_ids (List[str]): List of ids that should be returned by the all endpoint
-        user_ids (List[str]): List of ids that should be returned by the user endpoint
+        all_ids (List[Union[str, Dict]]): List of ids or configurations that should be returned by the all endpoint
+        user_ids (List[Union[str, Dict]]): List of ids or configurations that should be returned by the user endpoint
         uploaded (List): List that will be updated with uploaded instances
     """
     get_all = comms_calls["get_all"]
@@ -37,11 +37,14 @@ def mock_comms_entity_gets(
     get_instance = comms_calls["get_instance"]
     upload_instance = comms_calls["upload_instance"]
 
-    instances = [generate_fn(id=id) for id in all_ids]
-    user_instances = [generate_fn(id=id) for id in user_ids]
+    all_ents = [ent if type(ent) == dict else {"id": ent} for ent in all_ents]
+    user_ents = [ent if type(ent) == dict else {"id": ent} for ent in user_ents]
+
+    instances = [generate_fn(**ent) for ent in all_ents]
+    user_instances = [generate_fn(**ent) for ent in user_ents]
     mocker.patch.object(comms, get_all, return_value=instances)
     mocker.patch.object(comms, get_user, return_value=user_instances)
-    get_behavior = get_comms_instance_behavior(generate_fn, all_ids)
+    get_behavior = get_comms_instance_behavior(generate_fn, all_ents)
     mocker.patch.object(
         comms, get_instance, side_effect=get_behavior,
     )
@@ -49,22 +52,25 @@ def mock_comms_entity_gets(
     mocker.patch.object(comms, upload_instance, side_effect=upload_behavior)
 
 
-def get_comms_instance_behavior(generate_fn: Callable, ids: List[str]) -> Callable:
+def get_comms_instance_behavior(
+    generate_fn: Callable, ents: List[Union[str, Dict]]
+) -> Callable:
     """Function that defines a GET behavior
 
     Args:
         generate_fn (Callable): Function to generate entity dictionaries
-        ids (List[str]): List of IDs that are allowed to return
+        ents (List[Union[str, Dict]]): List of Entities configurations that are allowed to return
 
     Return:
         function: Function that returns an entity dictionary if found,
         or raises an error if not
     """
+    ids = [ent["id"] if type(ent) == dict else ent for ent in ents]
 
     def get_behavior(id: str):
         if id in ids:
-            id = str(id)
-            return generate_fn(id=id)
+            idx = ids.index(id)
+            return generate_fn(**ents[idx])
         else:
             raise CommunicationRetrievalError
 
