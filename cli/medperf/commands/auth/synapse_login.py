e4098d2fa4df7cda275c9eb67cb6885f9e528c1e
hasan7n <hasankassim7@hotmail.com>
2023-02-10 14:26:39 +0100

    add auth by access token

--- cli/medperf/commands/auth/synapse_login.py
+++ cli/medperf/commands/auth/synapse_login.py
@@ -2,9 +2,19 @@ from medperf import config
 import synapseclient
 
 
+def get_access_token(username, password):
+    import requests
+
+    url = "https://repo-prod.prod.sagebase.org/auth/v1/login2"
+    body = {"username": username, "password": password}
+    res = requests.post(url, json=body)
+    return res.json()["accessToken"]
+
+
 class SynapseLogin:
+    # TODO: decide
     @staticmethod
-    def run(username: str = None, password: str = None):
+    def run2(username: str = None, password: str = None):
         """Login to the Synapse server. Must be done only once.
         """
         username = username if username else config.ui.prompt("username: ")
@@ -12,3 +22,13 @@ class SynapseLogin:
 
         syn = synapseclient.Synapse()
         syn.login(username, password, rememberMe=True)
+
+    def run(username: str = None, password: str = None):
+        """Login to the Synapse server. Must be done only once.
+        """
+        username = username if username else config.ui.prompt("username: ")
+        password = password if password else config.ui.hidden_prompt("password: ")
+        access_token = get_access_token(username, password)
+
+        syn = synapseclient.Synapse()
+        syn.login(authToken=access_token, rememberMe=True)
