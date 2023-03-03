5ef86a2d3c9b1f78d6eaa49cc6e6be8f1e8df64e
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-03-03 16:56:57 -0500

    Fix empty folders paths

--- templates/data_preparator_mlcube/hooks/post_gen_project.py
+++ templates/data_preparator_mlcube/hooks/post_gen_project.py
@@ -5,16 +5,14 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 if __name__ == "__main__":
-    dir = "{{ cookiecutter.project_slug }}"
-
-    input_data_path = os.path.join(dir, "mlcube/workspace/input_data")
-    data_path = os.path.join(dir, "mlcube/workspace/data")
-    labels_path = os.path.join(dir, "mlcube/workspace/labels")
+    input_data_path = "mlcube/workspace/input_data"
+    data_path = "mlcube/workspace/data"
+    labels_path = "mlcube/workspace/labels"
 
     paths = [input_data_path, data_path, labels_path]
 
     if "{{ cookiecutter.use_separate_output_labels }}" == "y":
-        input_labels_path = os.path.join(dir, "mlcube/workspace/input_labels")
+        input_labels_path = "mlcube/workspace/input_labels"
         paths.append(input_labels_path)
 
     for path in paths:
