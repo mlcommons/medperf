6eebd59060a12a258ae165dfbf2e20a056afe102
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-03-03 16:54:04 -0500

    Create empty directories on hook

--- templates/data_preparator_mlcube/hooks/post_gen_project.py
+++ templates/data_preparator_mlcube/hooks/post_gen_project.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import os
-import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 if __name__ == "__main__":
-    if "{{ cookiecutter.use_separate_output_labels }}" != "y":
-        input_labels_path = os.path.join(
-            "{{ cookiecutter.project_slug }}", "mlcube/workspace/input_labels"
-        )
-        shutil.rmtree()
+    dir = "{{ cookiecutter.project_slug }}"
+
+    input_data_path = os.path.join(dir, "mlcube/workspace/input_data")
+    data_path = os.path.join(dir, "mlcube/workspace/data")
+    labels_path = os.path.join(dir, "mlcube/workspace/labels")
+
+    paths = [input_data_path, data_path, labels_path]
+
+    if "{{ cookiecutter.use_separate_output_labels }}" == "y":
+        input_labels_path = os.path.join(dir, "mlcube/workspace/input_labels")
+        paths.append(input_labels_path)
+
+    for path in paths:
+        os.makedirs(path, exist_ok=True)
