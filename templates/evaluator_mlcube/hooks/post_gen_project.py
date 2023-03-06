7338236411635e7581008b25b6465f5055d50e4f
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-03-06 12:09:20 -0500

    Remove reference to undefined template variable

--- templates/evaluator_mlcube/hooks/post_gen_project.py
+++ templates/evaluator_mlcube/hooks/post_gen_project.py
@@ -9,10 +9,5 @@ if __name__ == "__main__":
     labels_path = "mlcube/workspace/labels"
 
     paths = [preds_path, labels_path]
-
-    if "{{ cookiecutter.use_separate_output_labels }}" == "y":
-        input_labels_path = "mlcube/workspace/input_labels"
-        paths.append(input_labels_path)
-
     for path in paths:
         os.makedirs(path, exist_ok=True)
