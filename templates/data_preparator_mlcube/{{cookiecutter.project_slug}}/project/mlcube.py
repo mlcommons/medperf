4f78981d3a337e452b1bbcdcfb2fbe79c34b021b
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-03-07 17:55:41 -0500

    Ignore more flake8 errors

--- templates/data_preparator_mlcube/{{cookiecutter.project_slug}}/project/mlcube.py
+++ templates/data_preparator_mlcube/{{cookiecutter.project_slug}}/project/mlcube.py
@@ -11,9 +11,9 @@ def prepare(
     labels_path: str = typer.Option(..., "--labels_path"),
     parameters_file: str = typer.Option(..., "--parameters_file"),
     output_path: str = typer.Option(..., "--output_path"),
-    {% if cookiecutter.use_separate_output_labels == 'y' -%} # noqa: E999
+    {% if cookiecutter.use_separate_output_labels == 'y' -%}  # noqa: E999 E225
     output_labels_path: str = typer.Option(..., "--output_labels_path"),
-    {% endif %} # noqa: E999
+    {% endif %}  # noqa: E999 E225
 ):
     # Modify the prepare command as needed
     raise NotImplementedError("The prepare method is not yet implemented")
