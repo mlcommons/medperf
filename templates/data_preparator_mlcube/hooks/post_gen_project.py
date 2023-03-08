6bb60d0460a5f3b530bcf71afd0ee68d90a367d9
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2023-03-08 18:41:35 -0500

    Fix inconsistency with labels paths

--- templates/data_preparator_mlcube/hooks/post_gen_project.py
+++ templates/data_preparator_mlcube/hooks/post_gen_project.py
@@ -6,14 +6,14 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 if __name__ == "__main__":
     input_data_path = "mlcube/workspace/input_data"
+    input_labels_path = "mlcube/workspace/input_labels"
     data_path = "mlcube/workspace/data"
-    labels_path = "mlcube/workspace/labels"
 
-    paths = [input_data_path, data_path, labels_path]
+    paths = [input_data_path, input_labels_path, data_path]
 
     if "{{ cookiecutter.use_separate_output_labels }}" == "y":
-        input_labels_path = "mlcube/workspace/input_labels"
-        paths.append(input_labels_path)
+        labels_path = "mlcube/workspace/labels"
+        paths.append(labels_path)
 
     for path in paths:
         os.makedirs(path, exist_ok=True)
