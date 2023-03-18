e60407410b4056297b974e89a97285e26d495521
hasan7n <hasankassim7@hotmail.com>
2023-03-18 13:54:58 +0100

    fix tests

--- cli/medperf/tests/comms/entity_resources/test_resources.py
+++ cli/medperf/tests/comms/entity_resources/test_resources.py
@@ -1,48 +1,64 @@
 import os
+from medperf.utils import base_storage_path
 import pytest
 import medperf.config as config
 from medperf.comms.entity_resources import resources
+import yaml
 
 url = "https://mock.url"
 
 
-def test_get_cube_file_calls_download_resource(mocker):
-    # Arrange
-    cube_path = "path/to/cube"
-    path = "path"
-    filename = "filename"
-    mocker.patch.object(resources, "get_file_sha1", return_value="hash")
-    spy = mocker.patch.object(resources, "download_resource")
+@pytest.fixture
+def setup(mocker, fs):
+    def download_resource_side_effect(url, outpath):
+        fs.create_file(outpath, contents=url)
 
-    filepath = os.path.join(cube_path, path, filename)
-
-    # Act
-    resources.get_cube_file(url, cube_path, path, filename)
-
-    # Assert
-    spy.assert_called_once_with(url, filepath)
+    mocker.patch.object(
+        resources, "download_resource", side_effect=download_resource_side_effect
+    )
 
 
 @pytest.mark.parametrize(
     "url", ["https://localhost:8000/image.sif", "https://test.com/docker_image.tar.gz"]
 )
-def test_get_cube_image_retrieves_image_if_not_local(mocker, url):
+def test_get_cube_image_retrieves_image_if_not_local(mocker, url, setup, fs):
+    # Arrange
+    cube_path = "cube/1"
+    image_name = "some_name"
+    cube_yaml_path = os.path.join(cube_path, config.cube_filename)
+    fs.create_file(
+        cube_yaml_path, contents=yaml.dump({"singularity": {"image": image_name}})
+    )
+    exp_file = os.path.join(cube_path, config.image_path, image_name)
+    os.makedirs(base_storage_path(config.images_storage), exist_ok=True)
+
+    # Act
+    resources.get_cube_image(url, cube_path)
+
+    # Assert
+    assert os.path.exists(exp_file)
+
+
+@pytest.mark.parametrize(
+    "url", ["https://localhost:8000/image.sif", "https://test.com/docker_image.tar.gz"]
+)
+def test_get_cube_image_uses_cache_if_available(mocker, url, fs):
     # Arrange
     spy = mocker.patch.object(resources, "get_cube_file", return_value=("", ""))
     cube_path = "cube/1"
-    exp_filename = "some name"
-    mocker.patch("os.makedirs")
-    mocker.patch("os.path.exists", return_value=False)
-    mocker.patch("os.symlink")
-    mocker.patch("shutil.move")
-    mocker.patch.object(resources, "get_file_sha1", return_value="hash")
-    mocker.patch.object(resources, "get_cube_image_name", return_value=exp_filename)
+    image_name = "some_name"
+    cube_yaml_path = os.path.join(cube_path, config.cube_filename)
+    fs.create_file(
+        cube_yaml_path, contents=yaml.dump({"singularity": {"image": image_name}})
+    )
+    img_path = os.path.join(base_storage_path(config.images_storage), "hash")
+    fs.create_file(img_path, contents="img")
 
     # Act
-    resources.get_cube_image(url, cube_path, "hash")
+    resources.get_cube_image(url, "cube/1", "hash")
 
     # Assert
-    spy.assert_called_once_with(url, cube_path, config.image_path, exp_filename)
+    spy.assert_not_called()
 
 
 @pytest.mark.parametrize(
@@ -58,23 +74,3 @@ def test_get_cube_methods_run_get_cube_file(mocker, method):
 
     # Assert
     spy.assert_called_once()
-
-
-@pytest.mark.parametrize(
-    "url", ["https://localhost:8000/image.sif", "https://test.com/docker_image.tar.gz"]
-)
-def test_get_cube_image_uses_cache_if_available(mocker, url):
-    # Arrange
-    spy = mocker.patch.object(resources, "get_cube_file", return_value=("", ""))
-    mocker.patch("os.makedirs")
-    mocker.patch("os.path.exists", return_value=True)
-    mocker.patch("os.symlink")
-    mocker.patch("os.unlink")
-    mocker.patch.object(resources, "get_cube_image_name", return_value="some name")
-    mocker.patch.object(resources, "get_file_sha1", return_value="hash")
-
-    # Act
-    resources.get_cube_image(url, "cube/1", "hash")
-
-    # Assert
-    spy.assert_not_called()
