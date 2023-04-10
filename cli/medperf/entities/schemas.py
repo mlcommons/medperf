8022be2f6e7c09c75ab5c631961bfd0a2d32a2d0
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-04-10 09:52:15 -0500

    Implement formatted pydantic model

--- cli/medperf/entities/schemas.py
+++ cli/medperf/entities/schemas.py
@@ -1,11 +1,45 @@
 from datetime import datetime
-from pydantic import BaseModel, Field, validator, HttpUrl
+from pydantic import BaseModel, Field, validator, HttpUrl, ValidationError
 from typing import Optional
+from collections import defaultdict
 
 from medperf.enums import Status
+from medperf.exceptions import MedperfException
 
 
-class MedperfSchema(BaseModel):
+class FormattedBaseModel(BaseModel):
+    """Override the ValidationError procedure so we can
+    format the error message in our desired way
+    """
+
+    def __init__(self, *args, **kwargs):
+        try:
+            super().__init__(*args, **kwargs)
+        except ValidationError as e:
+            errors_dict = defaultdict(list)
+            for error in e.errors():
+                field = error["loc"]
+                msg = error["msg"]
+                errors_dict[field].append(msg)
+
+            error_msg = "Field Validation Error:"
+            for field, errors in errors_dict.items():
+                error_msg += "\n"
+                field = field[0]
+                error_msg += f"- {field}: "
+                if len(errors) == 1:
+                    # If a single error for a field is given, don't create a sublist
+                    error_msg += errors[0]
+                else:
+                    # Create a sublist otherwise
+                    for e_msg in errors:
+                        error_msg += "\n"
+                        error_msg += f"\t- {e_msg}"
+
+            raise MedperfException(error_msg)
+
+
+class MedperfSchema(FormattedBaseModel):
     id: Optional[int]
     name: str = Field(..., max_length=20)
     owner: Optional[int]
@@ -59,13 +93,13 @@ class MedperfSchema(BaseModel):
         use_enum_values = True
 
 
-class DeployableSchema(BaseModel):
+class DeployableSchema(FormattedBaseModel):
     # TODO: This must change after allowing edits
     state: str = "OPERATION"
     is_valid: bool = True
 
 
-class ApprovableSchema(BaseModel):
+class ApprovableSchema(FormattedBaseModel):
     approved_at: Optional[datetime]
     approval_status: Status = None
 
