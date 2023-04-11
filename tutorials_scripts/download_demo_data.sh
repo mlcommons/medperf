448434ff8516d3a8a8f0b2feae2f2934e53c9937
hasan7n <hasankassim7@hotmail.com>
2023-04-11 16:03:17 +0200

    add script for tutorial setups

---
+++ tutorials_scripts/download_demo_data.sh
@@ -0,0 +1,6 @@
+mkdir medperf_tutorial
+cd medperf_tutorial
+wget https://storage.googleapis.com/medperf-storage/mock_xrv_demo_data.tar.gz
+tar -xf xrv_demo_data.tar.gz
+rm xrv_demo_data.tar.gz
+rm paths.yaml
