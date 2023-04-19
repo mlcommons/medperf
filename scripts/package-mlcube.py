9c0d38f19a1f895644ab5f0bfb8e970701818232
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-04-19 11:39:31 -0500

    Implement initial validation logic

--- scripts/package-mlcube.py
+++ scripts/package-mlcube.py
@@ -1,7 +1,41 @@
 import os
+import yaml
 import argparse
 import tarfile
 import shutil
+
+PARAMS_FILE = "parameters.yaml"
+STATS_FILE = "statistics.yaml"
+RESULTS_FILE = "results.yaml"
+RUNNERS = ["docker", "singularity"]
+TASK_DEFINITIONS = {
+    "infer": {
+        "inputs": {"data_path": None, "parameters_file": PARAMS_FILE},
+        "outputs": {"output_path": None},
+    },
+    "prepare": {
+        "inputs": {
+            "data_path": None,
+            "labels_path": None,
+            "parameters_file": PARAMS_FILE,
+        },
+        "outputs": {"output_path": None},
+    },
+    "sanity_check": {"inputs": {"data_path": None, "parameters_file": PARAMS_FILE}},
+    "statistics": {
+        "inputs": {"data_path": None, "parameters_file": PARAMS_FILE},
+        "outputs": {"output_path": STATS_FILE},
+    },
+    "evaluate": {
+        "inputs": {"predictions": None, "labels": None, "parameters_file": PARAMS_FILE},
+        "outputs": {"output_path": RESULTS_FILE},
+    },
+}
+VALID_COMBINATIONS = [
+    {"prepare", "sanity_check", "statistics"},
+    {"infer",},
+    {"evaluate",},
+]
 
 
 def get_args():
@@ -49,10 +83,58 @@ def create_empty_file(path):
         pass
 
 
+def check_runners(mlcube):
+    # To avoid ambiguity, MLCubes should only have one explicit image
+    image_detected = False
+    if "docker" in mlcube:
+        print("Docker runner configuration identified")
+        if "image" in mlcube["docker"]:
+            image_detected = True
+            image_name = mlcube["docker"]["image"]
+            print("Remember to push your image to a registry with:")
+            print(f"\tdocker push {image_name}")
+
+    if "singularity" in mlcube:
+        print("Singularity runner configuration identified")
+        if "image" in mlcube["singularity"] and image_detected:
+            msg = (
+                "An ambiguous configuration has been detected. "
+                + "Only one image can be defined across runners"
+            )
+            raise RuntimeError(msg)
+
+
+def validate_tasks(mlcube):
+    tasks = set(mlcube["tasks"])
+    # Raise error if no valid task configuration was found
+    if not any([combination - tasks == 0 for combination in VALID_COMBINATIONS]):
+        msg = (
+            "No valid task combination was found. "
+            + "Could not infer any medperf MLCube type. "
+            + "Please refer to the documentation on building medperf MLCubes"
+        )
+        raise RuntimeError("Could not infer any MLCube type")
+
+    medperf_tasks = TASK_DEFINITIONS.keys()
+    for task, contents in tasks.items():
+        if task not in medperf_tasks:
+            continue
+        expected_parameters = medperf_tasks[task]
+        if "inputs" in expected_parameters:
+            for exp_input, exp_val in expected_parameters["inputs"].items():
+                
+
 def main():
     args = get_args()
     mlcube_path = args.mlcube
     mlcube_manifest_path = os.path.join(mlcube_path, "mlcube.yaml")
+
+    with open(mlcube_manifest_path, "r") as f:
+        mlcube = yaml.safe_load(f)
+
+    check_runners(mlcube)
+    validate_tasks(mlcube)
+
     workspace_path = os.path.join(mlcube_path, "workspace")
     parameters_path = os.path.join(workspace_path, "parameters.yaml")
     additional_path = os.path.join(workspace_path, "additional_files")
