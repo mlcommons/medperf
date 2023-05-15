4a75f817cc570e2ab9ac96e87aa6061c2609b56a
hasan7n <hasankassim7@hotmail.com>
2023-05-15 15:31:49 +0200

    update resources logic

--- cli/medperf/comms/entity_resources/resources.py
+++ cli/medperf/comms/entity_resources/resources.py
@@ -1,27 +1,21 @@
+"""haha"""
+
 import shutil
 import os
 import medperf.config as config
 from medperf.utils import (
     base_storage_path,
+    generate_tmp_path,
     get_cube_image_name,
-    get_file_sha1,
+    get_folder_sha1,
+    remove_path,
     storage_path,
-    generate_tmp_uid,
+    untar,
 )
 from .utils import download_resource
 
 
-def get_cube_file(url: str, cube_path: str, path: str, filename: str):
-    path = os.path.join(cube_path, path)
-    if not os.path.isdir(path):
-        os.makedirs(path, exist_ok=True)
-    filepath = os.path.join(path, filename)
-    download_resource(url, filepath)
-    hash = get_file_sha1(filepath)
-    return filepath, hash
-
-
-def get_cube(url: str, cube_path: str) -> str:
+def get_cube(url: str, cube_path: str, expected_hash: str = None) -> str:
     """Downloads and writes an mlcube.yaml file from the server
 
     Args:
@@ -31,11 +25,12 @@ def get_cube(url: str, cube_path: str) -> str:
     Returns:
         str: location where the mlcube.yaml file is stored locally.
     """
-    cube_file = config.cube_filename
-    return get_cube_file(url, cube_path, "", cube_file)
+    output_path = os.path.join(cube_path, config.cube_filename)
+    hash = download_resource(url, output_path, expected_hash)
+    return output_path, hash
 
 
-def get_cube_params(url: str, cube_path: str) -> str:
+def get_cube_params(url: str, cube_path: str, expected_hash: str = None) -> str:
     """Retrieves the cube parameters.yaml file from the server
 
     Args:
@@ -45,12 +40,12 @@ def get_cube_params(url: str, cube_path: str) -> str:
     Returns:
         str: Location where the parameters.yaml file is stored locally.
     """
-    ws = config.workspace_path
-    params_file = config.params_filename
-    return get_cube_file(url, cube_path, ws, params_file)
+    output_path = os.path.join(cube_path, config.workspace_path, config.params_filename)
+    hash = download_resource(url, output_path, expected_hash)
+    return output_path, hash
 
 
-def get_cube_image(url: str, cube_path: str, hash: str = None) -> str:
+def get_cube_image(url: str, cube_path: str, hash_value: str = None) -> str:
     """Retrieves and stores the image file from the server. Stores images
     on a shared location, and retrieves a cached image by hash if found locally.
     Creates a symbolic link to the cube storage.
@@ -68,33 +63,40 @@ def get_cube_image(url: str, cube_path: str, hash: str = None) -> str:
     image_cube_path = os.path.join(cube_path, image_path)
     os.makedirs(image_cube_path, exist_ok=True)
     image_cube_file = os.path.join(image_cube_path, image_name)
-    imgs_storage = base_storage_path(config.images_storage)
-
-    if not hash:
-        # No hash provided, we need to download the file first
-        _, local_hash = get_cube_file(url, cube_path, image_path, image_name)
-        img_storage = os.path.join(imgs_storage, local_hash)
-        shutil.move(image_cube_file, img_storage)
-    else:
-        img_storage = os.path.join(imgs_storage, hash)
-
-    if not os.path.exists(img_storage):
-        # If image doesn't exist locally, download it normally
-        # And move it to shared storage
-        _, local_hash = get_cube_file(url, cube_path, image_path, image_name)
-        shutil.move(image_cube_file, img_storage)
-
-    # Create a symbolic link to individual cube storage
     if os.path.exists(image_cube_file):
         # Remove existing links
         os.unlink(image_cube_file)
+
+    imgs_storage = base_storage_path(config.images_storage)
+    if not hash_value:
+        # No hash provided, we need to download the file first
+        hash_value = download_resource(url, image_cube_file)
+        img_storage = os.path.join(imgs_storage, hash_value)
+        shutil.move(image_cube_file, img_storage)
+    else:
+        img_storage = os.path.join(imgs_storage, hash_value)
+        if not os.path.exists(img_storage):
+            # If image doesn't exist locally, download it normally
+            download_resource(url, img_storage, hash_value)
+
+    # Create a symbolic link to individual cube storage
     os.symlink(img_storage, image_cube_file)
-    local_hash = get_file_sha1(img_storage)
-    return image_cube_file, local_hash
+    return image_cube_file, hash_value
 
 
-def get_cube_additional(url: str, cube_path: str) -> str:
+def get_cube_additional(
+    url: str,
+    cube_path: str,
+    expected_tarball_hash: str = None,
+    expected_folder_hash: str = None,
+) -> str:
     """Retrieves and stores the additional_files.tar.gz file from the server
+
+    Note: there is no scenario of having expected_tarball_hash == None and
+    expected_folder_hash != None, since passing the expected_folder_hash is
+    something controlled by the client not the user. If the client has
+    expected_tarball_hash == None, then there is no way the client has
+    expected_folder_hash != None.
 
     Args:
         url (str): URL where the additional_files.tar.gz file can be downloaded.
@@ -103,14 +105,28 @@ def get_cube_additional(url: str, cube_path: str) -> str:
     Returns:
         str: Location where the additional_files.tar.gz file is stored locally.
     """
-    add_path = config.additional_path
-    tball_file = config.tarball_filename
-    return get_cube_file(url, cube_path, add_path, tball_file)
+    additional_files_folder = os.path.join(cube_path, config.additional_path)
+
+    # If the additional_files folder exists, check if it has the expected hash
+    if expected_folder_hash and os.path.exists(additional_files_folder):
+        folder_hash = get_folder_sha1(additional_files_folder)
+        if folder_hash == expected_folder_hash:
+            return expected_tarball_hash, expected_folder_hash
+
+    # Remove the folder if it exists
+    remove_path(additional_files_folder)
+
+    # Download the tarball file as usual
+    output_path = os.path.join(additional_files_folder, config.tarball_filename)
+    tarball_hash = download_resource(url, output_path, expected_tarball_hash)
+
+    # Untar and keep track of the folder hash.
+    untar(output_path)
+    folder_hash = get_folder_sha1(additional_files_folder)
+    return tarball_hash, folder_hash
 
 
-def get_benchmark_demo_dataset(
-    demo_data_url: str, uid: str = generate_tmp_uid()
-) -> str:
+def get_benchmark_demo_dataset(url: str, expected_hash: str = None) -> str:
     """Downloads the benchmark demo dataset and stores it in the user's machine
 
     Args:
@@ -120,16 +136,15 @@ def get_benchmark_demo_dataset(
     Returns:
         str: path where the downloaded demo dataset can be found
     """
-    tmp_dir = storage_path(config.demo_data_storage)
-    demo_data_path = os.path.join(tmp_dir, uid)
-    tball_file = config.tarball_filename
-    filepath = os.path.join(demo_data_path, tball_file)
+    demo_storage = storage_path(config.demo_data_storage)
+    if expected_hash:
+        output_path = os.path.join(demo_storage, expected_hash, config.tarball_filename)
+        if not os.path.exists(output_path):
+            download_resource(url, output_path, expected_hash)
+    else:
+        tmp_output_path = generate_tmp_path()
+        expected_hash = download_resource(url, tmp_output_path)
+        output_path = os.path.join(demo_storage, expected_hash, config.tarball_filename)
+        os.rename(tmp_output_path, output_path)
 
-    # Don't re-download if something already exists with same uid
-    if os.path.exists(filepath):
-        return filepath
-
-    os.makedirs(demo_data_path, exist_ok=True)
-
-    download_resource(demo_data_url, filepath)
-    return filepath
+    return output_path, expected_hash
