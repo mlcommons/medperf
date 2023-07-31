ea984787cc9e3b3ce8b9bde30e513c24c2faf691
hasan7n <hasankassim7@hotmail.com>
2023-07-31 15:29:40 +0200

    modify tutorials scripts

--- tutorials_scripts/simulate_data_association_approval.sh
+++ tutorials_scripts/simulate_data_association_approval.sh
@@ -1,10 +1,3 @@
-SERVER_URL="https://localhost:8000"
-VERSION_PREFIX="/api/v0"
-BENCHMARK_OWNER="testbo@example.com"
-DATASET="1"
-BENCHMARK="1"
-
-LOGIN_SCRIPT="$(dirname $(dirname "$0"))/server/token_from_online_storage.py"
-
-BENCHMARK_OWNER_TOKEN=$(python $LOGIN_SCRIPT --email $BENCHMARK_OWNER)
-curl -sk -X PUT $SERVER_URL$VERSION_PREFIX/datasets/$DATASET/benchmarks/$BENCHMARK/ -d '{"approval_status": "APPROVED"}' -H 'Content-Type: application/json' -H "Authorization: Bearer $BENCHMARK_OWNER_TOKEN"
+medperf auth login -e testbo@example.com
+medperf association approve -b 1 -d 1
+medperf auth login -e testdo@example.com
