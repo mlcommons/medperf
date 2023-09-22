bb71e43cb0c45329d5bba6b62a6b55c9ace3b93d
hasan7n <hasankassim7@hotmail.com>
2023-09-22 16:23:54 +0200

    add /me/reports endpoint

--- server/utils/urls.py
+++ server/utils/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("datasets/", views.DatasetList.as_view()),
     path("mlcubes/", views.MlCubeList.as_view()),
     path("results/", views.ModelResultList.as_view()),
+    path("reports/", views.ReportList.as_view()),
     path("datasets/associations/", views.DatasetAssociationList.as_view()),
     path("mlcubes/associations/", views.MlCubeAssociationList.as_view()),
 ]
