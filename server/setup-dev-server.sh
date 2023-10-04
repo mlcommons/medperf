6a56d9a8ac6be99f18ead56dff500f209c748816
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-10-04 17:39:52 -0500

    Remove realpath dependency

--- server/setup-dev-server.sh
+++ server/setup-dev-server.sh
@@ -11,8 +11,8 @@ done
 
 DEPLOY="${DEPLOY:-1}"
 CERT_GENERATE="${CERT_GENERATE:-1}"
-CERT_FILE="${CERT_FILE:-$(realpath cert.crt)}"
-KEY_FILE="${KEY_FILE:-$(realpath cert.key)}"
+CERT_FILE="${CERT_FILE:-cert.crt}"
+KEY_FILE="${KEY_FILE:-cert.key}"
 RESET_DB="${RESET_DB:-0}"
 
 echo $CERT_FILE
