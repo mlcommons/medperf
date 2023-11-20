a9abb6d74cdfd153749aa3e51c7d5e450e8a5362
hasan7n <hasankassim7@hotmail.com>
2023-11-20 11:07:07 +0100

    change: fix PUT user permission PUT now is only permitted to the admin users We need to define what can be edited and how to sync with Auth0 before exposing this to the end users

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -4,6 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+# TODO: define what can be edited, and how. Keep in mind
+#       that some of these information is stored in Auth0
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
