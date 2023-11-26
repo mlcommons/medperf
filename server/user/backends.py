f4150ae2d501e06e930a5d596e3d72f84c29e1da
hasan7n <hasankassim7@hotmail.com>
2023-11-26 23:14:13 +0100

    add logic to save user email in medperf database

--- server/user/backends.py
+++ server/user/backends.py
@@ -1,9 +1,17 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import AuthenticationFailed, InvalidToken
 from rest_framework_simplejwt.settings import api_settings
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from drf_spectacular.plumbing import build_bearer_security_scheme_object
+
+
+def __get_email_from_token(validated_token):
+    try:
+        return validated_token[settings.TOKEN_USER_EMAIL_CLAIM]
+    except KeyError:
+        raise InvalidToken(_("Token must contain the user email address"))
 
 
 class JWTAuthenticateOrCreateUser(JWTAuthentication):
@@ -16,8 +24,9 @@ class JWTAuthenticateOrCreateUser(JWTAuthentication):
         try:
             user = self.user_model.objects.get(**{api_settings.USER_ID_FIELD: user_id})
         except self.user_model.DoesNotExist:
+            user_email = __get_email_from_token(validated_token)
             user = self.user_model.objects.create_user(
-                **{api_settings.USER_ID_FIELD: user_id}
+                **{api_settings.USER_ID_FIELD: user_id}, email=user_email
             )
 
         if not user.is_active:
