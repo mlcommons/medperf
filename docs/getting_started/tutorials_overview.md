cf87cd1974d2b55f9003b5a7de89cbc13bf73481
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-11-27 15:44:53 -0300

    feat: homepage changes, home button on documentation pages, cards on tutorial intro, sidebar indentation

--- docs/getting_started/tutorials_overview.md
+++ docs/getting_started/tutorials_overview.md
@@ -1,15 +1,27 @@
 # Overview
 
-To ensure users have the best experience in learning the fundamentals of MedPerf and utilizing the MedPerf client, the following set of tutorials are provided:
-
-- [How a Benchmark Committee can create and submit a benchmark.](benchmark_owner_demo.md)
-
-- [How a Model Owner can submit a model.](model_owner_demo.md)
-
-- [How a Data Owner can prepare their data and execute a benchmark.](data_owner_demo.md)
-
 The tutorials simulate a benchmarking example for the task of detecting thoracic diseases from chest X-ray scans. You can find the description of the used data [here](https://www.nih.gov/news-events/news-releases/nih-clinical-center-provides-one-largest-publicly-available-chest-x-ray-datasets-scientific-community). Throughout the tutorials, you will be interacting with a temporary local MedPerf server as described in the [setup page](setup.md). This allows you to freely experiment with the MedPerf client and rerun the tutorials as many times as you want, providing you with an immersive learning experience. Please note that these tutorials also serve as a general guidance to be followed when using the MedPerf client in a real scenario.
 
 Before proceeding to the tutorials, make sure you have the general tutorial environment [set up](setup.md).
 
+To ensure users have the best experience in learning the fundamentals of MedPerf and utilizing the MedPerf client, the following set of tutorials are provided:
+
+<div class="tutorial_card_container">
+  <a href="../benchmark_owner_demo" class="tutorial_card">
+    <h2>Benchmark <br>Committee</h2>
+    <p>Learn how a Benchmark Committee creates and submits a benchmark.</p>
+  </a>
+
+  <a href="../model_owner_demo" class="tutorial_card">
+    <h2>Model <br>Owner</h2>
+    <p>Learn how a Model Owner can submit a model.</p>
+  </a>
+
+  <a href="../data_owner_demo" class="tutorial_card">
+    <h2>Data <br>Owner</h2>
+    <p>Learn how to prepare their data and execute a benchmark.</p>
+  </a>
+</div>
+
 For a detailed reference on the commands used throughout the tutorials, you can always refer to the [command line interface documentation](../cli_reference.md).
+
--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,27 +1,3 @@
-const benchmarkElement = document.querySelector('#hands-on-tutorial-for-bechmark-committee');
-const dataElement = document.querySelector('#hands-on-tutorial-for-data-owners');
-const modelElement = document.querySelector('#hands-on-tutorial-for-model-owners');
-
-const containerElement = document.createElement('div');
-containerElement.classList.add('side-container');
-
-const imageElement = document.createElement('img');
-imageElement.src = '';
-imageElement.alt = '';
-
-imageElement.setAttribute('class', 'tutorial-image');
-imageElement.setAttribute('id', 'tutorial-image-1');
-
-containerElement.appendChild(imageElement);
-
-if (benchmarkElement) benchmarkElement.appendChild(containerElement);
-else if (dataElement) dataElement.appendChild(containerElement);
-else if (modelElement) modelElement.appendChild(containerElement);
-
-const sideContainer = document.querySelector('.side-container');
-
-const image = sideContainer.querySelector('img');
-
 const imageSources = [
   { start: 0, image: 1 },
   { start: 500, image: 2 },
@@ -41,13 +17,57 @@ const imageSources = [
   { start: 6880, image: 14 },
 ];
 
+// ADDS IMAGING SCROLL TO TUTORIALS
 window.addEventListener('scroll', function() {
+  const benchmarkElement = document.querySelector('#hands-on-tutorial-for-bechmark-committee');
+  const dataElement = document.querySelector('#hands-on-tutorial-for-data-owners');
+  const modelElement = document.querySelector('#hands-on-tutorial-for-model-owners');
+
+  const containerElement = document.createElement('div');
+  containerElement.classList.add('side-container');
+
+  const imageElement = document.createElement('img');
+  imageElement.src = '';
+  imageElement.alt = '';
+
+  imageElement.setAttribute('class', 'tutorial-image');
+  imageElement.setAttribute('id', 'tutorial-image-1');
+
+  containerElement.appendChild(imageElement);
+
+  if (benchmarkElement) benchmarkElement.appendChild(containerElement);
+  else if (dataElement) dataElement.appendChild(containerElement);
+  else if (modelElement) modelElement.appendChild(containerElement);
+
+  const sideContainer = document.querySelector('.side-container');
+  let image
+
+  if (sideContainer) image = sideContainer.querySelector('img');
+
   const scrollPosition = window.scrollY;
   let imageSrc = '';
-  imageSources.forEach(({ start, image}) => {
+  imageSources.forEach(({ start, image }) => {
     if (scrollPosition > start) {
       imageSrc = `https://raw.githubusercontent.com/gabrielraeder/website/main/docs/static/images/workflow/miccai-tutorial${image}.png`;
     }
   })
-  image.src = imageSrc;
+  if (image) {
+    image.src = imageSrc;
+  }
+});
+
+// ADDS HOME BUTTON TO HEADER
+document.addEventListener('DOMContentLoaded', function() {
+  const headerTitle = document.querySelector(".md-header__ellipsis");
+
+  if (headerTitle && window.location.pathname !== "/") {
+    const newElement = document.createElement("a");
+    newElement.textContent = "Home";
+    newElement.classList.add('header_home_btn');
+    newElement.href = "/";
+
+    headerTitle.appendChild(newElement);
+    console.log(document.querySelector(".header_home_btn"))
+    
+  }
 });
