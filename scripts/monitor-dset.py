08032460b77f4edeb08ee56ff6d5fc54220da8eb
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-01-08 14:16:39 -0500

    remove hardcoded subject-timepoint

--- scripts/monitor-dset.py
+++ scripts/monitor-dset.py
@@ -41,7 +41,7 @@ from textual.widgets import (
 DSET_HELP = "The Dataset to monitor. If and ID is passed, medperf will be used to identify the dataset. If a path is passed, it will look at that path instead"
 MLCUBE_HELP = "The Data Preparation MLCube UID used to create the data"
 STAGES_HELP = "Path to stages YAML file containing documentation about the Data Preparation stages"
-DEFAULT_SEGMENTATION = "AAAC_00_2008.12.17_duplicate_tumorMask_model_0.nii.gz"
+DEFAULT_SEGMENTATION = "tumorMask_model_0.nii.gz"
 DEFAULT_STAGES_PATH = os.path.join(os.path.dirname(__file__), "assets/stages.yaml")
 BRAINMASK = "brainMask_fused.nii.gz"
 REVIEW_FILENAME = "review_cases.tar.gz"
