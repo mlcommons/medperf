8cb57cd0f3879bc06bf1d9b075a8d7c71ba3e989
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-01-24 11:05:00 -0500

    Fix mlcube run tests

--- cli/medperf/tests/entities/test_cube.py
+++ cli/medperf/tests/entities/test_cube.py
@@ -161,7 +161,7 @@ class TestRun:
         )
         mocker.patch(PATCH_CUBE.format("Cube.get_config"), side_effect=["", ""])
         expected_cmd = (
-            f"mlcube run --mlcube={self.manifest_path} --task={task} "
+            f"mlcube --log-level critical run --mlcube={self.manifest_path} --task={task} "
             + f"--platform={self.platform} --network=none --mount=ro"
             + ' -Pdocker.cpu_args="-u $(id -u):$(id -g)"'
             + ' -Pdocker.gpu_args="-u $(id -u):$(id -g)"'
@@ -183,7 +183,7 @@ class TestRun:
             side_effect=["", ""],
         )
         expected_cmd = (
-            f"mlcube run --mlcube={self.manifest_path} --task={task} "
+            f"mlcube --log-level critical run --mlcube={self.manifest_path} --task={task} "
             + f"--platform={self.platform} --network=none"
             + ' -Pdocker.cpu_args="-u $(id -u):$(id -g)"'
             + ' -Pdocker.gpu_args="-u $(id -u):$(id -g)"'
@@ -202,7 +202,7 @@ class TestRun:
         spy = mocker.patch("pexpect.spawn", side_effect=mpexpect.spawn)
         mocker.patch(PATCH_CUBE.format("Cube.get_config"), side_effect=["", ""])
         expected_cmd = (
-            f"mlcube run --mlcube={self.manifest_path} --task={task} "
+            f"mlcube --log-level critical run --mlcube={self.manifest_path} --task={task} "
             + f'--platform={self.platform} --network=none --mount=ro test="test"'
             + ' -Pdocker.cpu_args="-u $(id -u):$(id -g)"'
             + ' -Pdocker.gpu_args="-u $(id -u):$(id -g)"'
@@ -224,7 +224,7 @@ class TestRun:
             side_effect=["cpuarg cpuval", "gpuarg gpuval"],
         )
         expected_cmd = (
-            f"mlcube run --mlcube={self.manifest_path} --task={task} "
+            f"mlcube --log-level critical run --mlcube={self.manifest_path} --task={task} "
             + f"--platform={self.platform} --network=none --mount=ro"
             + ' -Pdocker.cpu_args="cpuarg cpuval -u $(id -u):$(id -g)"'
             + ' -Pdocker.gpu_args="gpuarg gpuval -u $(id -u):$(id -g)"'
