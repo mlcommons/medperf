9db84eabdcf0b0b4545f67c21da15f3a82fc9f9f
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-03-14 15:10:55 -0500

    apply auto-formatting

--- scripts/package-logs.py
+++ scripts/package-logs.py
@@ -3,11 +3,13 @@ from medperf import config
 from medperf.init import initialize
 import tarfile
 
+
 def main():
     initialize()
 
     with tarfile.open("medperf_logs.tar.gz", "w:gz") as tar:
         tar.add(config.logs_folder, arcname=os.path.basename(config.logs_folder))
+
 
 if __name__ == "__main__":
     main()
