8a0ee9140c80f64483888401f5f38384b458abd1
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-03-19 09:10:35 -0500

    fix linting issues

--- cli/medperf/logging/utils.py
+++ cli/medperf/logging/utils.py
@@ -249,6 +249,7 @@ def log_machine_details():
 
     logging.debug(yaml.dump(debug_dict, default_flow_style=False))
 
+
 def package_logs():
     if not os.path.exists(config.logs_folder):
         return
