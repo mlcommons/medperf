864f59731ef326d72084fade32008cdad1144110
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-03-27 14:47:28 -0500

    Fix and simplify log_machine_details function

--- cli/medperf/logging/utils.py
+++ cli/medperf/logging/utils.py
@@ -16,175 +16,176 @@ from medperf import config
 
 
 def get_system_information():
-    # Get basic system information
-    system_info = {
-        "Platform": platform.platform(),
-        "Hostname": socket.gethostname(),
-        "Processor": platform.processor(),
-        "System Version": platform.version(),
-        "Python Version": platform.python_version(),
-    }
-    return system_info
+    try:
+        # Get basic system information
+        system_info = {
+            "Platform": platform.platform(),
+            "Hostname": socket.gethostname(),
+            "Processor": platform.processor(),
+            "System Version": platform.version(),
+            "Python Version": platform.python_version(),
+        }
+        return system_info
+    except Exception:
+        return traceback.format_exc()
 
 
 def get_memory_usage():
-    # Get memory usage
-    memory = psutil.virtual_memory()
-    return {
-        "Total Memory": memory.total,
-        "Available Memory": memory.available,
-        "Used Memory": memory.used,
-        "Memory Usage Percentage": memory.percent,
-    }
+    try:
+        # Get memory usage
+        memory = psutil.virtual_memory()
+        return {
+            "Total Memory": memory.total,
+            "Available Memory": memory.available,
+            "Used Memory": memory.used,
+            "Memory Usage Percentage": memory.percent,
+        }
+    except Exception:
+        return traceback.format_exc()
 
 
 def get_disk_usage():
-    # Get disk usage
-    paths_of_interest = [os.environ["HOME"], config.config_storage]
-    disk_usage_dict = {}
-    for poi in paths_of_interest:
-        try:
-            disk_usage_poi = psutil.disk_usage(poi)
-        except OSError:
-            disk_usage_poi = "NOT FOUND"
-        disk_usage_dict[poi] = {
-            "Total Disk Space": disk_usage_poi.total,
-            "Used Disk Space": disk_usage_poi.used,
-            "Free Disk Space": disk_usage_poi.free,
-            "Disk Usage Percentage": disk_usage_poi.percent,
-        }
+    try:
+        # Get disk usage
+        paths_of_interest = [os.environ["HOME"], config.config_storage]
+        disk_usage_dict = {}
+        for poi in paths_of_interest:
+            try:
+                disk_usage_poi = psutil.disk_usage(poi)
+            except OSError:
+                disk_usage_poi = "NOT FOUND"
+            disk_usage_dict[poi] = {
+                "Total Disk Space": disk_usage_poi.total,
+                "Used Disk Space": disk_usage_poi.used,
+                "Free Disk Space": disk_usage_poi.free,
+                "Disk Usage Percentage": disk_usage_poi.percent,
+            }
 
-    return disk_usage_dict
+        return disk_usage_dict
+    except Exception:
+        return traceback.format_exc()
 
 
 def get_configuration_variables():
-    config_vars = vars(config)
-    config_dict = {}
-    for item in dir(config):
-        if item.startswith("__"):
-            continue
-        config_dict[item] = config_vars[item]
-    config_dict = filter_var_dict_for_yaml(config_dict)
-    return config_dict
+    try:
+        config_vars = vars(config)
+        config_dict = {}
+        for item in dir(config):
+            if item.startswith("__"):
+                continue
+            config_dict[item] = config_vars[item]
+        config_dict = filter_var_dict_for_yaml(config_dict)
+        return config_dict
+    except Exception:
+        return traceback.format_exc()
 
 
 def filter_var_dict_for_yaml(unfiltered_dict):
-    valid_types = (str, dict, list, int, float)
-    filtered_dict = {}
-    for key, value in unfiltered_dict.items():
-        if not isinstance(value, valid_types) and value is not None:
-            try:
-                value = str(value)
-            except Exception:
-                value = "<OBJECT>"
-        if isinstance(value, dict):
-            value = filter_var_dict_for_yaml(value)
-        filtered_dict[key] = value
+    try:
+        valid_types = (str, dict, list, int, float)
+        filtered_dict = {}
+        for key, value in unfiltered_dict.items():
+            if not isinstance(value, valid_types) and value is not None:
+                try:
+                    value = str(value)
+                except Exception:
+                    value = "<OBJECT>"
+            if isinstance(value, dict):
+                value = filter_var_dict_for_yaml(value)
+            filtered_dict[key] = value
 
-    return filtered_dict
+        return filtered_dict
+    except Exception:
+        return traceback.format_exc()
 
 
 def get_storage_contents():
-    storage_paths = config.storage.copy()
-    storage_paths["credentials_folder"] = {
-        "base": os.path.dirname(config.creds_folder),
-        "name": os.path.basename(config.creds_folder),
-    }
-    ignore_paths = {"datasets_folder", "predictions_folder", "results_folder"}
-    contents = {}
+    try:
+        storage_paths = config.storage.copy()
+        storage_paths["credentials_folder"] = {
+            "base": os.path.dirname(config.creds_folder),
+            "name": os.path.basename(config.creds_folder),
+        }
+        ignore_paths = {"datasets_folder", "predictions_folder", "results_folder"}
+        contents = {}
 
-    for pathname, path in storage_paths.items():
-        if pathname in ignore_paths:
-            contents[pathname] = "<REDACTED>"
-            continue
-        full_path = os.path.join(path["base"], path["name"])
+        for pathname, path in storage_paths.items():
+            if pathname in ignore_paths:
+                contents[pathname] = "<REDACTED>"
+                continue
+            full_path = os.path.join(path["base"], path["name"])
+            p = subprocess.Popen(
+                ["ls", "-lR", full_path],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            output, _ = p.communicate()
+            if p.returncode != 0:
+                contents[pathname] = "Could not retrieve tree for storage paths"
+            contents[pathname] = output
+
+        return contents
+    except Exception:
+        return traceback.format_exc()
+
+
+def get_installed_packages():
+    try:
+        installed_packages = {}
+        for dist in metadata.distributions():
+            installed_packages[dist.name] = dist.version
+        return installed_packages
+    except Exception:
+        return traceback.format_exc()
+
+
+def get_python_environment_information():
+    try:
+        environment_info = {
+            "Operating System": platform.system(),
+            "Operating System Release": platform.release(),
+            "Operating System Version": platform.version(),
+            "Python Implementation": platform.python_implementation(),
+            "Python Version": platform.python_version(),
+            "Python Compiler": platform.python_compiler(),
+            "Python Build": platform.python_build(),
+            "Machine Architecture": platform.machine(),
+            "Processor Type": platform.processor(),
+            "Python Executable": sys.executable,
+            "Installed Modules": get_installed_packages(),
+        }
+        return environment_info
+    except Exception:
+        return traceback.format_exc()
+
+
+def get_additional_information():
+    try:
+        sh_script = os.path.join(os.path.dirname(__file__), "get_host_info.sh")
         p = subprocess.Popen(
-            ["ls", "-lR", full_path],
+            ["bash", sh_script],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
         )
         output, _ = p.communicate()
-        if p.returncode != 0:
-            contents[pathname] = "Could not retrieve tree for storage paths"
-        contents[pathname] = output
-
-    return contents
-
-
-def get_installed_packages():
-    installed_packages = {}
-    for dist in metadata.distributions():
-        installed_packages[dist.name] = dist.version
-    return installed_packages
-
-
-def get_python_environment_information():
-    environment_info = {
-        "Operating System": platform.system(),
-        "Operating System Release": platform.release(),
-        "Operating System Version": platform.version(),
-        "Python Implementation": platform.python_implementation(),
-        "Python Version": platform.python_version(),
-        "Python Compiler": platform.python_compiler(),
-        "Python Build": platform.python_build(),
-        "Machine Architecture": platform.machine(),
-        "Processor Type": platform.processor(),
-        "Python Executable": sys.executable,
-        "Installed Modules": get_installed_packages(),
-    }
-    return environment_info
-
-
-def get_additional_information():
-    sh_script = os.path.join(os.path.dirname(__file__), "get_host_info.sh")
-    p = subprocess.Popen(
-        ["bash", sh_script],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    )
-    output, _ = p.communicate()
-    return output
+        return output
+    except Exception:
+        return traceback.format_exc()
 
 
 def log_machine_details():
     system_info = {}
-    try:
-        system_info["System Info"] = get_system_information()
-    except Exception:
-        system_info["System Info"] = traceback.format_exc()
 
-    try:
-        system_info["Memory Usage"]
-    except Exception:
-        system_info["Memory Usage"] = traceback.format_exc()
+    system_info["System Info"] = get_system_information()
+    system_info["Memory Usage"] = get_memory_usage()
+    system_info["Disk Usage"] = get_disk_usage()
+    system_info["Medperf Configuration"] = get_configuration_variables()
+    system_info["Medperf Storage Contents"] = get_storage_contents()
+    system_info["Python Environment"] = get_python_environment_information()
 
-    try:
-        system_info["Disk Usage"]
-    except Exception:
-        system_info["Disk Usage"] = traceback.format_exc()
-
-    try:
-        system_info["Medperf Configuration"]
-    except Exception:
-        system_info["Medperf Configuration"] = traceback.format_exc()
-
-    try:
-        system_info["Medperf Storage Contents"]
-    except Exception:
-        system_info["Medperf Storage Contents"] = traceback.format_exc()
-
-    try:
-        system_info["Python Environment"]
-    except Exception:
-        system_info["Python Environment"] = traceback.format_exc()
-
-    try:
-        add_info = get_additional_information()
-    except Exception:
-        add_info = traceback.format_exc()
-
+    add_info = get_additional_information()
     debug_dict = {"Machine Details": system_info}
 
     logging.debug(yaml.dump(debug_dict, default_flow_style=False))
