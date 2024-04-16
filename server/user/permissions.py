db33adadc48cc87bedeb321c38d3880298da8b25
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-04-16 15:30:50 -0500

    Implement permission for mlcube owners to see dset emails

--- server/user/permissions.py
+++ server/user/permissions.py
@@ -17,3 +17,14 @@ class IsOwnUser(BasePermission):
             return True
         else:
             return False
+
+
+class IsOwnerOfUsedMLCube(BasePermission):
+    def has_permission(self, request, view):
+        pk = view.kwargs.get("pk", None)
+        if not pk:
+            return False
+        
+        user_datasets_using_owned_mlcube = Dataset.objects.filter(owner=pk, data_preparation_mlcube__owner=request.user)
+        
+        return len(user_datasets_using_owned_mlcube)
