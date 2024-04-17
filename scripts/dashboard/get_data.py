ebd778804dcc33ada7ea7b662c4e637b8b49ae11
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2024-04-16 20:08:05 -0500

    Remove unnecessary try-except block

--- scripts/dashboard/get_data.py
+++ scripts/dashboard/get_data.py
@@ -32,17 +32,14 @@ def stage_id2name(stage_str, stages_df):
 
 def get_institution_from_email(email, user2institution):
     invalid_institutions = {"gmail.com", "hotmail.com", "yahoo.com", "outlook.com"}
-    try:
-        institution = user2institution.get(email, None)
-        if institution is not None:
-            return institution
-        plausible_institution = email.split("@")[1]
-        if plausible_institution in invalid_institutions:
-            return email
-        else:
-            return plausible_institution
-    except IndexError:
-        return np.nan
+    institution = user2institution.get(email, None)
+    if institution is not None:
+        return institution
+    plausible_institution = email.split("@")[1]
+    if plausible_institution in invalid_institutions:
+        return email
+    else:
+        return plausible_institution
 
 
 def build_dset_df(dsets, user2institution, stages_df):
