36c4ddcafc0973c00a253d42151b814acaa1172b
hasan7n <hasankassim7@hotmail.com>
2024-04-18 16:24:24 +0200

    add ca server entity

---
+++ server/ca/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CA(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=300)
+    port = models.IntegerField()
+    fingerprint = models.TextField()
+    metadata = models.JSONField(default=dict, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
+
+    class Meta:
+        ordering = ["created_at"]
