7b9b3a14534b617cfe2a1a3d5006ada2be9947f7
hasan7n <hasankassim7@hotmail.com>
2024-04-22 23:27:40 +0200

    modify training event

--- server/trainingevent/serializers.py
+++ server/trainingevent/serializers.py
@@ -21,6 +21,16 @@ class EventSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "User cannot create a new event unless the previous event has finished"
             )
+        aggregator = training_exp.aggregator
+        if not aggregator:
+            raise serializers.ValidationError(
+                "User cannot create a new event if the experiment has no aggregator"
+            )
+        plan = training_exp.plan
+        if plan is None:
+            raise serializers.ValidationError(
+                "User cannot create a new event if the experiment has no plan"
+            )
 
         return data
 
