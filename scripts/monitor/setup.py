1a9483d90fb486eed5921f650b3cdd07de823e4d
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2024-06-07 15:26:51 -0500

    Specify versions for rano monitor dependencies (#594)
    * Specify dependencies versions
    
    * Remove dependency typo
    
    * Increase rano-monitor version

--- scripts/monitor/setup.py
+++ scripts/monitor/setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="rano-monitor",
-    version="0.0.0",
+    version="0.0.1",
     description="TUI for monitoring medperf datasets",
     url="https://github.com/mlcommons/medperf",
     author="MLCommons",
