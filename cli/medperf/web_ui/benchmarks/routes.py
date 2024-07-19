ca356ccd3d20151610d6b9a1daccc9d12147722e
Viacheslav Kukushkin <vy.kukushkin@gmail.com>
2024-07-19 20:54:23 +0300

    added links to mlcube

--- cli/medperf/web_ui/benchmarks/routes.py
+++ cli/medperf/web_ui/benchmarks/routes.py
@@ -7,6 +7,7 @@ from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
 from medperf.entities.benchmark import Benchmark
+from medperf.entities.cube import Cube
 from medperf.account_management import get_medperf_user_data
 
 router = APIRouter()
@@ -30,4 +31,17 @@ def benchmarks_ui(request: Request, local_only: bool = False, mine_only: bool = 
 @router.get("/ui/{benchmark_id}", response_class=HTMLResponse)
 def benchmark_detail_ui(request: Request, benchmark_id: int):
     benchmark = Benchmark.get(benchmark_id)
-    return templates.TemplateResponse("benchmark_detail.html", {"request": request, "benchmark": benchmark})
+    data_preparation_mlcube = Cube.get(cube_uid=benchmark.data_preparation_mlcube)
+    reference_model_mlcube = Cube.get(cube_uid=benchmark.reference_model_mlcube)
+    metrics_mlcube = Cube.get(cube_uid=benchmark.data_evaluator_mlcube)
+
+    return templates.TemplateResponse(
+        "benchmark_detail.html",
+        {
+            "request": request,
+            "benchmark": benchmark,
+            "data_preparation_mlcube": data_preparation_mlcube,
+            "reference_model_mlcube": reference_model_mlcube,
+            "metrics_mlcube": metrics_mlcube
+        }
+    )
