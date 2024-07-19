55fe60e44cd99d15858d231ebddf23f2966fbe17
Viacheslav Kukushkin <vy.kukushkin@gmail.com>
2024-07-19 11:39:31 +0300

    Aded mlcube detailed page

--- cli/medperf/web_ui/mlcubes/routes.py
+++ cli/medperf/web_ui/mlcubes/routes.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
@@ -24,3 +24,9 @@ def mlcubes_ui(request: Request, local_only: bool = False, mine_only: bool = Fal
         filters=filters,
     )
     return templates.TemplateResponse("mlcubes.html", {"request": request, "mlcubes": mlcubes})
+
+
+@router.get("/ui/{mlcube_id}", response_class=HTMLResponse)
+def mlcube_detail_ui(request: Request, mlcube_id: int):
+    mlcube = Cube.get(cube_uid=mlcube_id)
+    return templates.TemplateResponse("mlcube_detail.html", {"request": request, "mlcube": mlcube})
