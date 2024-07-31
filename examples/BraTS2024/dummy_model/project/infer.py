484cfdd2cc04cb28661034a7f732a0476f6b4dbd
verena <9377970+vpchung@users.noreply.github.com>
2024-07-30 17:29:57 -0700

    add "logic" for model inferences

--- examples/BraTS2024/dummy_model/project/infer.py
+++ examples/BraTS2024/dummy_model/project/infer.py
@@ -1,30 +1,17 @@
 import os
 import shutil
 
+from numpy.random import randint
+import pandas as pd
+
 
 def run_inference(data_path, parameters, output_path):
     task = parameters["task"]
-    if task == "segmentation":
+    if task == "segmentation-radiotherapy":
         for k in os.listdir(data_path):
-            file = os.path.join(data_path, k, f"{k}-t1c.nii.gz")
+            file = os.path.join(data_path, k, f"{k}_t1c.nii.gz")
             shutil.copyfile(file, os.path.join(output_path, f"{k}.nii.gz"))
-
-    elif task == "inpainting":
-        for k in os.listdir(data_path):
-            file = os.path.join(data_path, k, f"{k}-mask.nii.gz")
-            shutil.copyfile(
-                file, os.path.join(output_path, f"{k}-t1n-inference.nii.gz")
-            )
     else:
-        modalities = ["t1c", "t1n", "t2f", "t2w"]
-        for k in os.listdir(data_path):
-            found = []
-            for file in os.listdir(os.path.join(data_path, k)):
-                s1 = len("MMM.nii.gz")
-                s2 = len(".nii.gz")
-                found.append(file[-s1:-s2])
-
-            missing = list(set(modalities).difference(found))[0]
-            dummy = "t1c" if missing != "t1c" else "t2f"
-            file = os.path.join(data_path, k, f"{k}-{dummy}.nii.gz")
-            shutil.copyfile(file, os.path.join(output_path, f"{k}-{missing}.nii.gz"))
+        dummy_predictions = pd.DataFrame({'SubjectID': os.listdir(data_path)})
+        dummy_predictions["Prediction"] = randint(6,size=len(os.listdir(data_path)))
+        dummy_predictions.to_csv(os.path.join(output_path, "predictions.csv"), index=False)
