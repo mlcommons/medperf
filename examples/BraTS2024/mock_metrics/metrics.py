a8fed11825266e244126e6889c0b72004318006c
verena <9377970+vpchung@users.noreply.github.com>
2024-08-09 02:22:43 -0700

    add mock metrics for adult glioma post-op

--- examples/BraTS2024/mock_metrics/metrics.py
+++ examples/BraTS2024/mock_metrics/metrics.py
@@ -6,27 +6,52 @@ and BraTS-Path 2024.
 import os
 from glob import glob
 
+import numpy as np
 import pandas as pd
+import SimpleITK as sitk
 import yaml
 from cnb_tools import validation_toolkit as vtk
+
+
+def check_image_dims(path):
+    base_size = np.array([182, 218, 182])
+    image = sitk.ReadImage(path)
+    size_array = np.array(image.GetSize())
+    assert (base_size == size_array).all(), (
+        "Image size is not [182, 218, 182] for " + path
+    )
+
+
+def _check_counts(pred, gold):
+    if len(pred) != len(gold):
+        raise ValueError("Predictions number don't match labels")
+
+    if sorted(pred) != sorted(gold):
+        raise ValueError("Predictions don't match submission criteria")
+
+
+def check_for_postop(labels, predictions):
+    label_id_len = len("xxxxx-xxx-seg.nii.gz")
+    subjectids = [
+        label[-label_id_len:].replace("-seg", "") for label in os.listdir(labels)
+    ]
+
+    pred_id_len = len("xxxxx-xxx.nii.gz")
+    pred_subjectids = [pred[-pred_id_len:] for pred in os.listdir(predictions)]
+    _check_counts(pred_subjectids, subjectids)
+
+    for pred in os.listdir(predictions):
+        check_image_dims(os.path.join(predictions, pred))
 
 
 def check_for_radiotherapy(labels, predictions):
     label_id_len = len("xxxx-x_gtv.nii.gz")
     subjectids = [
-        label[-label_id_len:].replace("_gtv", "")
-        for label
-        in os.listdir(labels)
+        label[-label_id_len:].replace("_gtv", "") for label in os.listdir(labels)
     ]
-
     pred_id_len = len("xxxx-x.nii.gz")
     pred_subjectids = [pred[-pred_id_len:] for pred in os.listdir(predictions)]
-
-    if len(pred_subjectids) != len(subjectids):
-        raise ValueError("Predictions number don't match labels")
-
-    if sorted(pred_subjectids) != sorted(subjectids):
-        raise ValueError("Predictions don't match submission criteria")
+    _check_counts(pred_subjectids, subjectids)
 
 
 def check_for_pathology(parent):
@@ -57,7 +82,9 @@ def check_for_pathology(parent):
 def calculate_metrics(labels, predictions, parameters, output_path):
     task = parameters["task"]
 
-    if task == "segmentation-radiotherapy":
+    if task == "segmentation":
+        check_for_postop(labels, predictions)
+    elif task == "segmentation-radiotherapy":
         check_for_radiotherapy(labels, predictions)
     else:
         check_for_pathology(predictions)
