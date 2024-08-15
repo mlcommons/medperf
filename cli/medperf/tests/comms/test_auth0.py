61863163f87d4ac2a13b2955e2f8776266da5617
Alejandro Aristizábal <alejandro.aristizabal24@gmail.com>
2024-08-15 06:53:47 -0500

    Check for refresh token absolute expiration time (#604)
    * Handle expired tokens
    
    * Implement tests for token expiration
    
    * Use absolute expiration time instead of expiration time
    
    * Fix style issues
    
    * refresh token expiration: bugfix and migration
    
    * fix local auth bug
    
    * don't logout if access token not expired
    
    ---------
    
    Co-authored-by: hasan7n <hasankassim7@hotmail.com>
    Co-authored-by: hasan7n <78664424+hasan7n@users.noreply.github.com>

--- cli/medperf/tests/comms/test_auth0.py
+++ cli/medperf/tests/comms/test_auth0.py
@@ -2,8 +2,11 @@ import time
 from unittest.mock import ANY
 from medperf.tests.mocks import MockResponse
 from medperf.comms.auth.auth0 import Auth0
+from medperf import config
+from medperf.exceptions import AuthenticationError
 import sqlite3
 import pytest
+
 
 PATCH_AUTH = "medperf.comms.auth.auth0.{}"
 
@@ -35,6 +38,7 @@ def test_token_is_not_refreshed_if_not_expired(mocker, setup):
         "access_token": "",
         "token_expires_in": 900,
         "token_issued_at": time.time(),
+        "logged_in_at": time.time(),
     }
     mocker.patch(PATCH_AUTH.format("read_credentials"), return_value=creds)
     spy = mocker.patch(PATCH_AUTH.format("Auth0._Auth0__refresh_access_token"))
@@ -48,17 +52,44 @@ def test_token_is_not_refreshed_if_not_expired(mocker, setup):
 
 def test_token_is_refreshed_if_expired(mocker, setup):
     # Arrange
+    expiration_time = 900
+    mocked_issued_at = time.time() - expiration_time
     creds = {
         "refresh_token": "",
         "access_token": "",
-        "token_expires_in": 900,
-        "token_issued_at": time.time() - 1000,
+        "token_expires_in": expiration_time,
+        "token_issued_at": mocked_issued_at,
+        "logged_in_at": time.time(),
     }
     mocker.patch(PATCH_AUTH.format("read_credentials"), return_value=creds)
     spy = mocker.patch(PATCH_AUTH.format("Auth0._Auth0__refresh_access_token"))
 
     # Act
     Auth0().access_token
+
+    # Assert
+    spy.assert_called_once()
+
+
+def test_logs_out_if_session_reaches_token_absolute_expiration_time(mocker, setup):
+    # Arrange
+    expiration_time = 900
+    absolute_expiration_time = config.token_absolute_expiry
+    mocked_logged_in_at = time.time() - absolute_expiration_time
+    mocked_issued_at = time.time() - expiration_time
+    creds = {
+        "refresh_token": "",
+        "access_token": "",
+        "token_expires_in": expiration_time,
+        "token_issued_at": mocked_issued_at,
+        "logged_in_at": mocked_logged_in_at,
+    }
+    mocker.patch(PATCH_AUTH.format("read_credentials"), return_value=creds)
+    spy = mocker.patch(PATCH_AUTH.format("Auth0.logout"))
+
+    # Act
+    with pytest.raises(AuthenticationError):
+        Auth0().access_token
 
     # Assert
     spy.assert_called_once()
