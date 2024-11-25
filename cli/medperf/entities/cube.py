fb7e598773ceffcec6635b4d23e4743c48dc41d9
hasan7n <hasankassim7@hotmail.com>
2024-11-25 14:55:24 +0100

    refactor mlcube.run

--- cli/medperf/entities/cube.py
+++ cli/medperf/entities/cube.py
@@ -18,6 +18,7 @@ from medperf.exceptions import InvalidArgumentError, ExecutionError, InvalidEnti
 import medperf.config as config
 from medperf.comms.entity_resources import resources
 from medperf.account_management import get_medperf_user_data
+from medperf.entities.cube_utils import craft_cube_command, get_config
 
 
 class Cube(Entity, DeployableSchema):
@@ -249,88 +250,17 @@ class Cube(Entity, DeployableSchema):
             read_protected_input (bool, optional): Wether to disable write permissions on input volumes. Defaults to True.
             kwargs (dict): additional arguments that are passed directly to the mlcube command
         """
-        # TODO: refactor this function. Move things to MLCube if possible
         kwargs.update(string_params)
-        cmd = f"mlcube --log-level {config.loglevel} run"
-        cmd += (
-            f' --mlcube="{self.cube_path}" --task={task} --platform={config.platform}'
+        cmd = craft_cube_command(
+            cube_path=self.cube_path,
+            task=task,
+            read_protected_input=read_protected_input,
+            kwargs=kwargs,
+            env_dict=env_dict,
+            port=port,
+            publish_on=publish_on,
+            converted_singularity_image_name=self._converted_singularity_image_name,
         )
-        if task not in [
-            "train",
-            "start_aggregator",
-            "trust",
-            "get_client_cert",
-            "get_server_cert",
-            "get_experiment_status",
-            "add_collaborator",
-            "remove_collaborator",
-            "update_plan",
-        ]:
-            cmd += " --network=none"
-        if config.gpus is not None:
-            cmd += f" --gpus={config.gpus}"
-        if read_protected_input:
-            cmd += " --mount=ro"
-        for k, v in kwargs.items():
-            cmd_arg = f'{k}="{v}"'
-            cmd = " ".join([cmd, cmd_arg])
-
-        container_loglevel = config.container_loglevel
-        if container_loglevel:
-            env_dict["MEDPERF_LOGLEVEL"] = container_loglevel.upper()
-
-        env_args_string = ""
-        for key, val in env_dict.items():
-            env_args_string += f"--env {key}={val} "
-        env_args_string = env_args_string.strip()
-
-        # TODO: we should override run args instead of what we are doing below
-        #       we shouldn't allow arbitrary run args unless our client allows it
-        if config.platform == "docker":
-            # use current user
-            cpu_args = self.get_config("docker.cpu_args") or ""
-            gpu_args = self.get_config("docker.gpu_args") or ""
-            cpu_args = " ".join([cpu_args, "-u $(id -u):$(id -g)"]).strip()
-            gpu_args = " ".join([gpu_args, "-u $(id -u):$(id -g)"]).strip()
-            if port is not None:
-                if publish_on:
-                    cpu_args += f" -p {publish_on}:{port}:{port}"
-                    gpu_args += f" -p {publish_on}:{port}:{port}"
-                else:
-                    cpu_args += f" -p {port}:{port}"
-                    gpu_args += f" -p {port}:{port}"
-            cmd += f' -Pdocker.cpu_args="{cpu_args}"'
-            cmd += f' -Pdocker.gpu_args="{gpu_args}"'
-            if env_args_string:  # TODO: why MLCube UI is so brittle?
-                env_args = self.get_config("docker.env_args") or ""
-                env_args = " ".join([env_args, env_args_string]).strip()
-                cmd += f' -Pdocker.env_args="{env_args}"'
-
-        elif config.platform == "singularity":
-            # use -e to discard host env vars, -C to isolate the container (see singularity run --help)
-            run_args = self.get_config("singularity.run_args") or ""
-            run_args = " ".join([run_args, "-eC"]).strip()
-            run_args += " " + env_args_string
-            cmd += f' -Psingularity.run_args="{run_args}"'
-            # TODO: check if ports are already exposed. Think if this is OK
-            # TODO: check about exposing to specific network interfaces
-            # TODO: check if --env works
-
-            # set image name in case of running docker image with singularity
-            # Assuming we only accept mlcube.yamls with either singularity or docker sections
-            # TODO: make checks on submitted mlcubes
-            singularity_config = self.get_config("singularity")
-            if singularity_config is None:
-                cmd += (
-                    f' -Psingularity.image="{self._converted_singularity_image_name}"'
-                )
-        else:
-            raise InvalidArgumentError("Unsupported platform")
-
-        # set accelerator count to zero to avoid unexpected behaviours and
-        # force mlcube to only use --gpus to figure out GPU config
-        cmd += " -Pplatform.accelerator_count=0"
-
         logging.info(f"Running MLCube command: {cmd}")
         with spawn_and_kill(cmd, timeout=timeout) as proc_wrapper:
             proc = proc_wrapper.proc
@@ -357,7 +287,9 @@ class Cube(Entity, DeployableSchema):
             str: the path as specified in the mlcube.yaml file for the desired
                 output for the desired task. Defaults to None if out_key not found
         """
-        out_path = self.get_config(f"tasks.{task}.parameters.outputs.{out_key}")
+        out_path = get_config(
+            self.cube_path, f"tasks.{task}.parameters.outputs.{out_key}"
+        )
         if out_path is None:
             return
 
@@ -374,26 +306,6 @@ class Cube(Entity, DeployableSchema):
             out_path = os.path.join(out_path, params[param_key])
 
         return out_path
-
-    def get_config(self, identifier):
-        """
-        Returns the output parameter specified in the mlcube.yaml file
-
-        Args:
-            identifier (str): `.` separated keys to traverse the mlcube dict
-        Returns:
-            str: the parameter value, None if not found
-        """
-        with open(self.cube_path, "r") as f:
-            cube = yaml.safe_load(f)
-
-        keys = identifier.split(".")
-        for key in keys:
-            if key not in cube:
-                return
-            cube = cube[key]
-
-        return cube
 
     def display_dict(self):
         return {
