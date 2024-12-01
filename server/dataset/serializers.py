3078f415b3654f0a8f544d5323d9bdcfa4264b48
Mohammad Kassem <mohammadkassem54.mk@gmail.com>
2024-12-01 22:54:33 +0000

    fix generated_uid bug

--- server/dataset/serializers.py
+++ server/dataset/serializers.py
@@ -28,28 +28,6 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["owner"]
 
-    def validate_state(self, state):
-        # NOTE: this is checking the uniqueness of generated_uid across
-        #       operational datasets. This check relies on the fact that
-        #       such a constraint can only be violated by updating the state.
-        #       updating the generated_uid while state is OPERATION is already
-        #       not allowed.
-        # NOTE: This is written explicitely although the constraint is also
-        #       defined in models.py. The reason is that DRF doesn't translate
-        #       uniqueness constraint correctly, causing a 500 server error
-        #       if the check was left to be done by the database.
-        if state == "OPERATION" and self.instance.state == "DEVELOPMENT":
-            constraint = (
-                Dataset.objects.all()
-                .filter(state="OPERATION", generated_uid=self.instance.generated_uid)
-                .exists()
-            )
-            if constraint:
-                raise serializers.ValidationError(
-                    "An Operational dataset with the same generated UID already exists"
-                )
-        return state
-
     def validate(self, data):
         if self.instance.state == "OPERATION":
             editable_fields = ["is_valid", "user_metadata"]
@@ -59,4 +37,33 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
                         raise serializers.ValidationError(
                             "User cannot update non editable fields in Operation mode"
                         )
+
+        # NOTE: this is checking the uniqueness of generated_uid across
+        #       operational datasets. This check relies on the fact that
+        #       such a constraint can only be violated by updating the state.
+        #       updating the generated_uid while state is OPERATION is already
+        #       not allowed.
+        # NOTE: This is written explicitely although the constraint is also
+        #       defined in models.py. The reason is that DRF doesn't translate
+        #       uniqueness constraint correctly, causing a 500 server error
+        #       if the check was left to be done by the database.
+        if (
+            data.get("state")
+            and data["state"] == "OPERATION"
+            and self.instance.state == "DEVELOPMENT"
+        ):
+            if (
+                Dataset.objects.all()
+                .filter(
+                    state="OPERATION",
+                    generated_uid=data.get(
+                        "generated_uid", self.instance.generated_uid
+                    ),
+                )
+                .exists()
+            ):
+                raise serializers.ValidationError(
+                    "An Operational dataset with the same "
+                    "generated UID already exists"
+                )
         return data
