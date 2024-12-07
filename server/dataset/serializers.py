305214361b6918011bbae2eae27712378c1b9c78
Mohammad Kassem <mohammadkassem54.mk@gmail.com>
2024-12-07 17:00:11 +0000

    code refactoring

--- server/dataset/serializers.py
+++ server/dataset/serializers.py
@@ -28,16 +28,7 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["owner"]
 
-    def validate(self, data):
-        if self.instance.state == "OPERATION":
-            editable_fields = ["is_valid", "user_metadata"]
-            for k, v in data.items():
-                if k not in editable_fields:
-                    if v != getattr(self.instance, k):
-                        raise serializers.ValidationError(
-                            "User cannot update non editable fields in Operation mode"
-                        )
-
+    def _validate_guid(self, data):
         # NOTE: this is checking the uniqueness of generated_uid across
         #       operational datasets. This check relies on the fact that
         #       such a constraint can only be violated by updating the state.
@@ -52,7 +43,7 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
             and data["state"] == "OPERATION"
             and self.instance.state == "DEVELOPMENT"
         ):
-            if (
+            constraint = (
                 Dataset.objects.all()
                 .filter(
                     state="OPERATION",
@@ -61,9 +52,22 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
                     ),
                 )
                 .exists()
-            ):
+            )
+            if constraint:
                 raise serializers.ValidationError(
                     "An Operational dataset with the same "
                     "generated UID already exists"
                 )
+
+    def validate(self, data):
+        if self.instance.state == "OPERATION":
+            editable_fields = ["is_valid", "user_metadata"]
+            for k, v in data.items():
+                value_changed = v != getattr(self.instance, k)
+                if k not in editable_fields and value_changed:
+                    raise serializers.ValidationError(
+                        "User cannot update non editable fields in Operation mode"
+                    )
+            self._validate_guid(data)
+
         return data
