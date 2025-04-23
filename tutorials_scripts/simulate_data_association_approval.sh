957fee3488c339c9a2e53b5d86ea018116e10f22
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-04-23 11:22:03 -0300

    Fix user for association approval simulation

--- tutorials_scripts/simulate_data_association_approval.sh
+++ tutorials_scripts/simulate_data_association_approval.sh
@@ -1,3 +1,3 @@
-sh tutorials_scripts/medperf_login.sh testdo@example.com
+sh tutorials_scripts/medperf_login.sh testbo@example.com
 medperf association approve -b 1 -d 1
 sh tutorials_scripts/medperf_login.sh testdo@example.com
