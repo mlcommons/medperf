8a869445ebb27946a64d321e16db834728ad82f4
mhmdk0 <mohammadkassim54.mk@gmail.com>
2025-05-02 18:31:49 +0300

    fix dataset/model association cancellation for web-ui

--- cli/medperf/commands/dataset/associate.py
+++ cli/medperf/commands/dataset/associate.py
@@ -3,7 +3,7 @@ from medperf.entities.dataset import Dataset
 from medperf.entities.benchmark import Benchmark
 from medperf.utils import dict_pretty_print, approval_prompt
 from medperf.commands.result.create import BenchmarkExecution
-from medperf.exceptions import InvalidArgumentError
+from medperf.exceptions import CleanExit, InvalidArgumentError
 
 
 class AssociateDataset:
@@ -48,4 +48,4 @@ class AssociateDataset:
             metadata = {"test_result": result.results}
             comms.associate_dset(dset.id, benchmark_uid, metadata)
         else:
-            ui.print("Dataset association operation cancelled.")
+            raise CleanExit("Dataset association operation cancelled")
