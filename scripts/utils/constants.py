d69b4217d3aefb1b8fd524a19bb8e514edce289c
hasan7n <hasankassim7@hotmail.com>
2025-05-06 11:39:42 +0200

    remove outdated script

--- scripts/utils/constants.py
+++
@@ -1,6 +0,0 @@
-WSPACE_PATH = "workspace"
-ADD_PATH = "additional_files"
-ADD_FILE = "additional_files.tar.gz"
-MLCUBE_FILE = "mlcube.yaml"
-PARAMS_FILE = "parameters.yaml"
-RUNNERS = ["docker", "singularity"]
