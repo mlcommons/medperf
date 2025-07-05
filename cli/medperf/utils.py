6453ed59b563c130360439e21dbc2f58db95635c
hasan7n <hasankassim7@hotmail.com>
2025-07-05 15:21:39 +0200

    add command to view the URL and token for webui

--- cli/medperf/utils.py
+++ cli/medperf/utils.py
@@ -21,7 +21,7 @@ from colorama import Fore, Style
 from pexpect.exceptions import TIMEOUT
 from git import Repo, GitCommandError
 import medperf.config as config
-from medperf.exceptions import ExecutionError, InvalidArgumentError
+from medperf.exceptions import CleanExit, ExecutionError, InvalidArgumentError
 
 
 def get_file_hash(path: str) -> str:
@@ -543,3 +543,20 @@ def sanitize_path(path: str) -> str:
         raise InvalidArgumentError(f"Unaccepted path: {resolved_path}")
 
     return resolved_path
+
+
+def get_webui_properties():
+    if not os.path.exists(config.webui_host_props):
+        raise CleanExit("Web UI properties file could not be found.")
+
+    with open(config.webui_host_props) as f:
+        props = yaml.safe_load(f)
+
+    # print security token to CLI (avoid logging to file)
+    print("=" * 40)
+    print()
+    print("Use security token to view the web-UI:")
+    print(props["security_token"])
+    print()
+    print("=" * 40)
+    print(f"URL: http://{props['host']}:{props['port']}")
