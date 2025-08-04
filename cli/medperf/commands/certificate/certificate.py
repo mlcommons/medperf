c29f95b75c6e615de1a693514f0be3fc84a6fe4c
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-08-04 13:48:31 -0300

    Update CLI with get_client_certificate allowing a container_id

--- cli/medperf/commands/certificate/certificate.py
+++ cli/medperf/commands/certificate/certificate.py
@@ -4,6 +4,7 @@ import medperf.config as config
 from medperf.decorators import clean_except
 from medperf.commands.certificate.client_certificate import GetUserCertificate
 from medperf.commands.certificate.server_certificate import GetServerCertificate
+from medperf.exceptions import InvalidArgumentError
 
 app = typer.Typer()
 
@@ -12,17 +13,31 @@ app = typer.Typer()
 @clean_except
 def get_client_certificate(
     training_exp_id: int = typer.Option(
-        ...,
+        None,
         "--training_exp_id",
         "-t",
-        help="UID of training exp which you intend to be a part of",
+        help="UID of training exp which you intend to be a part of. Only one of training_exp_ip or container_id must be provided.",
+    ),
+    container_id: int = typer.Option(
+        None,
+        "--container-id",
+        "--container_id",
+        "-c",
+        help="UID of the Private container you intend to use. Only one of training_exp_ip or container_id must be provided.",
     ),
     overwrite: bool = typer.Option(
         False, "--overwrite", help="Overwrite cert and key if present"
     ),
 ):
     """get a client certificate"""
-    GetUserCertificate.run(training_exp_id, overwrite)
+    if training_exp_id is not None and container_id is not None:
+        raise InvalidArgumentError(
+            "Only one of training_exp_id or container_id may be provided!"
+        )
+
+    GetUserCertificate.run(
+        training_exp_id=training_exp_id, container_id=container_id, overwrite=overwrite
+    )
     config.ui.print("✅ Done!")
 
 
