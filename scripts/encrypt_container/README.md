31b9812e2791669b4ca10fe26304c4f654f0628a
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-08-14 08:22:34 -0300

    Add simple script for encrypting containers

---
+++ scripts/encrypt_container/README.md
@@ -0,0 +1,2 @@
+The provided scripts are used simply for testing and demo purposes.
+**THESE SCRIPTS SHOULD NOT BE TAKEN AS CRIPTOGRAPHICALLY SAFE!!**
---
+++ scripts/encrypt_container/encrypt_utils.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+
+
+import os
+
+
+def generate_fernet_key_and_encrypt(
+        file_to_encrypt: os.PathLike,
+        encrypted_image_output_path: os.PathLike
+        ) -> tuple[bytes, Fernet]:
+    key = Fernet.generate_key()
+    fernet_obj = Fernet(key)
+
+    with (
+        open(file_to_encrypt, 'rb') as original,
+        open(encrypted_image_output_path, 'wb') as encrypted
+        ):
+        encrypted_bytes = fernet_obj.encrypt(original.read())
+        encrypted.write(encrypted_bytes)
+
+    return key
