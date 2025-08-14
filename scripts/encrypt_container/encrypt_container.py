4d8cefadf077b76439b72b4ac1cebf36e3660a33
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-08-14 09:02:11 -0300

    linting

--- scripts/encrypt_container/encrypt_container.py
+++ scripts/encrypt_container/encrypt_container.py
@@ -12,49 +12,44 @@ app = typer.Typer()
 @app.command('generate_key_and_encrypt_container')
 def generate_key_and_encrypt(
     key_output_path: Path = typer.Option(
-        ..., 
+        ...,
         '-k',
         '--key-output-path',
-        dir_okay=False, 
-        writable=True, 
+        dir_okay=False,
+        writable=True,
         help='Output path for generate Fernet key'),
     docker_image_name: str = typer.Option(
-        None, 
+        None,
         '-d',
         '--docker-image',
-        help=(
-            'Name (including tag) of Docker image to encrypt.'
-            ' Must be present locally, will not be pulled.'
-            )
-        ),
+        help='Name (including tag) of Docker image to encrypt. '
+             'Must be present locally, will not be pulled.'),
     singularity_image_file: Path = typer.Option(
-        None, 
+        None,
         '-s',
         '-a',
         '--singularity-image',
         '--apptainer-image',
-        dir_okay=False, 
-        readable=True, 
-        exists=True, 
+        dir_okay=False,
+        readable=True,
+        exists=True,
         help='Singularity/Apptainer image to encrypt.'),
     encrypted_image_output_path: Path = typer.Option(
-        ..., 
+        ...,
         '-e',
         '--encrypted-image-output-path',
         dir_okay=False,
-        writable=True, 
-        help='Output path for encrypted container image'
-        ),
+        writable=True,
+        help='Output path for encrypted container image'),
 ):
     """
-    Generates a Fernet key and encrypts a Docker or Singularity image. 
+    Generates a Fernet key and encrypts a Docker or Singularity image.
     Exactly one of 'docker_image_name' or 'singularity_image_file' MUST be provided!
     """
-    if (
-        (docker_image_name is None and singularity_image_file is None) or
-        (docker_image_name is not None and singularity_image_file is not None)
-        ):
-        raise ValueError("Exactly one of 'docker_image_name' or 'singularity_image_file' MUST be provided!")
+    if (docker_image_name is None and singularity_image_file is None) or \
+       (docker_image_name is not None and singularity_image_file is not None):
+        raise ValueError("Exactly one of 'docker_image_name' or "
+                         "'singularity_image_file' MUST be provided!")
 
     with tempfile.TemporaryDirectory() as tmp_dir:
 
@@ -67,10 +62,11 @@ def generate_key_and_encrypt(
             subprocess.run(['docker', 'save', docker_image_name, '-o', docker_archive_file])
             file_to_encrypt = docker_archive_file
 
-        key = generate_fernet_key_and_encrypt(file_to_encrypt=file_to_encrypt, 
+        key = generate_fernet_key_and_encrypt(file_to_encrypt=file_to_encrypt,
                                               encrypted_image_output_path=encrypted_image_output_path)
         with open(key_output_path, 'wb') as key_file:
             key_file.write(key)
+
 
 if __name__ == '__main__':
     app()
