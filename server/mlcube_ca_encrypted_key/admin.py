fec54757fc6edc445088c7e1f4492e87f92c5b2f
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-09-02 16:37:50 -0300

    Rename admin panel class for clarity

--- server/mlcube_ca_encrypted_key/admin.py
+++ server/mlcube_ca_encrypted_key/admin.py
@@ -3,5 +3,5 @@ from .models import ModelCAEncryptedKey
 
 
 @admin.register(ModelCAEncryptedKey)
-class ContainerCAAdmin(admin.ModelAdmin):
+class ModelCAEncryptedKeyAdmin(admin.ModelAdmin):
     list_display = [field.name for field in ModelCAEncryptedKey._meta.fields]
