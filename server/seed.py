e757ba1b088e21066f6a919e23e79610aefc5593
hasan7n <hasankassim7@hotmail.com>
2025-09-15 22:12:24 +0200

    fix line length problem

--- server/seed.py
+++ server/seed.py
@@ -17,6 +17,7 @@ REPO_BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def populate_mock_benchmarks(api_server, admin_token):
+    demo_url = "https://storage.googleapis.com/medperf-storage/chestxray_tutorial/demo_data.tar.gz"
     for name, description in [
         ("Mock Brain Tumors Segmentation", "Mock benchmark for development purposes"),
         ("Mock Skin Cancer Detection", "Mock benchmark for development purposes"),
@@ -29,7 +30,7 @@ def populate_mock_benchmarks(api_server, admin_token):
                 "name": name,
                 "description": description,
                 "docs_url": "",
-                "demo_dataset_tarball_url": "https://storage.googleapis.com/medperf-storage/chestxray_tutorial/demo_data.tar.gz",
+                "demo_dataset_tarball_url": demo_url,
                 "demo_dataset_tarball_hash": "71faabd59139bee698010a0ae3a69e16d97bc4f2dde799d9e187b94ff9157c00",
                 "demo_dataset_generated_uid": "730d2474d8f22340d9da89fa2eb925fcb95683e0",
                 "data_preparation_mlcube": 1,
