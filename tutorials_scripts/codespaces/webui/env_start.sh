26c80100c59610a4a7da77b1cb1287cc7eec3ff3
hasan7n <hasankassim7@hotmail.com>
2025-09-18 11:21:04 +0200

    tmp

---
+++ tutorials_scripts/codespaces/webui/env_start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Preparing local medperf server..."
+# we are located at /workspaces/medperf/ where repo is cloned to
+pip install -e ./cli
+pip install -r server/requirements.txt
+pip install -r server/test-requirements.txt
+medperf profile activate local
+
+bash tutorials_scripts/setup_webui_tutorial.sh
+cd server
+cp .env.local.local-auth.sqlite .env
+
+echo "Medperf is ready for local usage"
+cd ../medperf_tutorial
