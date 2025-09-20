f3ec84737953332b125f5ccd3f8d629b2b45f4bf
hasan7n <hasankassim7@hotmail.com>
2025-09-20 13:38:42 +0200

    update codespaces script

--- tutorials_scripts/codespaces/training/env_start.sh
+++ tutorials_scripts/codespaces/training/env_start.sh
@@ -8,6 +8,9 @@ medperf profile activate local
 
 bash tutorials_scripts/setup_training_tutorial.sh
 
+# Update the configured server ip address
+sed -i "s/^  - name: 192.*/  - name: $(hostname -I | cut -d' ' -f 1)/" medperf_tutorial/training_config/secure_project.yml
+
 cd server
 cp .env.local.local-auth.sqlite .env
 cd ..
