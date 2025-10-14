ae92eef22ccdcd37cd222ee458f6b873f29883af
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-10-14 09:58:18 -0300

    Fix display of Container Config and Parameters file in WebUI for new format

--- cli/medperf/web_ui/yaml_fetch/routes.py
+++ cli/medperf/web_ui/yaml_fetch/routes.py
@@ -1,4 +1,3 @@
-import os.path
 from enum import Enum
 from fastapi import APIRouter, HTTPException, Depends
 from medperf.entities.cube import Cube
@@ -12,8 +11,8 @@ router = APIRouter()
 
 
 class FieldsToFetch(Enum):
-    container_yaml = "git_mlcube_url"
-    param_yaml = "git_parameters_url"
+    container_config = "container_config"
+    parameters_config = "parameters_config"
     dataset_report_path = "report_path"
     dataset_statistics_path = "statistics_path"
 
@@ -37,24 +36,9 @@ def fetch_yaml(
         }[entity]
 
         container = entity_class.get(entity_uid)
-        yaml_uri = getattr(container, field_to_fetch.value)
-        if yaml_uri.startswith("http"):
-            # some URL
-            response = requests.get(yaml_uri)
-            response.raise_for_status()  # Check if the request was successful
-            content = response.text
-        elif os.path.exists(yaml_uri):
-            # local file
-            with open(yaml_uri, "r") as file:
-                content = file.read()
-        else:
-            raise HTTPException(status_code=400, detail=f"Invalid YAML URL: {yaml_uri}")
-        # Validate YAML content
-        try:
-            yaml.safe_load(content)
-        except yaml.YAMLError:
-            raise HTTPException(status_code=400, detail="Invalid YAML content")
-
+        json_content = getattr(container, field_to_fetch.value)
+        content = yaml.safe_dump(json_content)
+        print(f'{content=}')
         return {"content": content}
     except requests.RequestException:
         raise HTTPException(status_code=400, detail="Failed to fetch YAML content")
