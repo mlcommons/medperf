43e0ef38f1f90451bbda94bc38a02881e8f2078b
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-11-07 09:20:48 -0300

    Improvements to airflow_parser

--- cli/medperf/containers/parsers/airflow_parser.py
+++ cli/medperf/containers/parsers/airflow_parser.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Tuple
+from typing import Dict, Union
 from medperf.exceptions import InvalidContainerSpec
 from medperf.containers.parsers.parser import Parser
 
@@ -15,9 +15,12 @@ class AirflowParser(Parser):
         self.allowed_runners = allowed_runners
 
         # The following variables are set when calling check_schema for the first time
-        self._steps = None
+        self._steps = []
         self.pools = None
-        self.step_ids = None
+        self.step_ids = []
+        self.container_ids = (
+            []
+        )  # TODO currently assumes only images on some registry, does not support files
 
     def check_schema(self) -> str:
         """
@@ -27,7 +30,6 @@ class AirflowParser(Parser):
             raise InvalidContainerSpec("Airflow config should have a 'steps' field.")
 
         self._steps = self.airflow_config["steps"]
-        self.step_ids = []
 
         final_step_candidates = []
         error_msgs = []
@@ -36,20 +38,19 @@ class AirflowParser(Parser):
         for i, step in enumerate(self._steps):
             self.step_ids.append(step["id"])
 
-            # Check if has minimal fields
-            missing_fields_msg = self._check_mandatory_fields(step=step, step_index=i)
-            if missing_fields_msg is not None:
-                error_msgs.append(missing_fields_msg)
-                continue
+            self._check_mandatory_fields(
+                step=step, step_index=i, error_msg_list=error_msgs
+            )
 
-            # Check last step
-            last_step_is_partioned_msg, is_last_step_candidate = self._check_last_step(
-                step=step
+            is_last_step_candidate = self._check_last_step(
+                step=step, error_msg_list=error_msgs
             )
             if is_last_step_candidate:
                 final_step_candidates.append(step["id"])
-            if last_step_is_partioned_msg:
-                error_msgs.append(last_step_is_partioned_msg)
+
+            container_image = self._verify_container(step, error_msg_list=error_msgs)
+            if container_image is not None:
+                self.container_ids.append(container_image)
 
             if "limit" in step.keys():
                 tmp_pools.update(self._create_pool_info(step))
@@ -69,27 +70,29 @@ class AirflowParser(Parser):
 
         self.pools = tmp_pools or None
 
+    @staticmethod
     def _check_mandatory_fields(
-        step: Dict[str, str], step_index: int
+        step: Dict[str, str], step_index: int, error_msg_list: list[str]
     ) -> Union[str, None]:
-        mandatory_fields = set("id", "type", "command")
+        mandatory_fields = {"id", "type"}
         missing_fields = mandatory_fields.difference(set(step.keys()))
         if missing_fields:
             ordered_fields = sorted(list(missing_fields))
+            if step.get("id"):
+                step_identifier = f"step {step['id']}"
+            else:
+                step_identifier = f"{step_index+1}th step"
             msg = (
-                f"The {step_index+1}th step in the yaml file is missing the "
+                f"The {step_identifier} in the yaml file is missing the "
                 f"following mandatory fields: ', '.join({ordered_fields})"
             )
-            return msg
-        else:
-            return None
+            error_msg_list.append(msg)
 
-    def _check_last_step(
-        step: Dict[str, str], step_index: int
-    ) -> Tuple[Union[str, None], bool]:
+    @staticmethod
+    def _check_last_step(step: Dict[str, str], error_msg_list: list[str]) -> bool:
         is_marked_as_last = step.get("last", False)
         inferred_as_last = step.get("next") is None
-        error_msg = None
+
         if is_marked_as_last or inferred_as_last:
             is_last_step_candidate = True
             if step.get("per_subject", False):
@@ -100,10 +103,11 @@ class AirflowParser(Parser):
                 error_msg += (
                     "You may need to add a final dummy step to join all results."
                 )
+                error_msg_list.append(error_msg)
         else:
             is_last_step_candidate = False
 
-        return error_msg, is_last_step_candidate
+        return is_last_step_candidate
 
     @staticmethod
     def _create_pool_info(step: Dict[str, Union[str, int]]):
@@ -116,3 +120,38 @@ class AirflowParser(Parser):
                 "parallel executions",
             }
         }
+
+    @staticmethod
+    def _verify_container(step: Dict[str, str], error_msg_list: list[str]):
+        if step.get("type") != "container":
+            return None
+
+        if "command" not in step:
+            msg = f"Step {step['id']} is of type 'container' bu does not specify a 'command' field!"
+            error_msg_list.append(msg)
+
+        try:
+            return step["image"]
+        except KeyError:
+            msg = f"Step {step['id']} is of type 'container' but does not specify a 'image' field!!"
+            error_msg_list.append(msg)
+            return None
+
+    # TODO validate how to add these methods to this parser
+    def check_task_schema(self, task):
+        pass
+
+    def get_setup_args(self):
+        pass
+
+    def get_volumes(self, task: str, medperf_mounts: dict):
+        pass
+
+    def get_run_args(self, task: str, medperf_mounts: dict):
+        pass
+
+    def is_report_specified(self):
+        pass
+
+    def is_metadata_specified(self):
+        pass
