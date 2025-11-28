8d308947c1cc884eef95302de6013ac01ebb1681
mhmdk0 <mohammadkassim54.mk@gmail.com>
2025-11-28 23:38:46 +0200

    fix e2e test - compatibility test

--- cli/medperf/web_ui/tests/pages/container/compatibility_test_page.py
+++ cli/medperf/web_ui/tests/pages/container/compatibility_test_page.py
@@ -9,9 +9,11 @@ class CompatibilityTestPage(BasePage):
     CONTAINER = (By.ID, "container-path")
     RUN_TEST_BTN = (By.ID, "run-comp-test-btn")
     CONTINUE_BTN = (By.CSS_SELECTOR, "button.btn-success")
+    NOT_ENCRYPTED = (By.ID, "without-encryption")
 
     def run_test(self, benchmark, container_path):
         self.select_by_text(self.BENCHMARK, benchmark)
         self.type(self.CONTAINER, container_path)
+        self.click(self.NOT_ENCRYPTED)
 
         self.click(self.RUN_TEST_BTN)
