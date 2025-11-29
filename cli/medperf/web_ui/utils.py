55810932b48aa544f7110e9634f9e0679206778a
mhmdk0 <mohammadkassim54.mk@gmail.com>
2025-11-29 06:10:05 +0200

    fix get_container_type

--- cli/medperf/web_ui/utils.py
+++ cli/medperf/web_ui/utils.py
@@ -1,12 +1,11 @@
 import uuid
 from medperf.entities.cube import Cube
-import yaml
 
 
 def get_container_type(container: Cube):
-    with open(container.cube_path, "r") as f:
-        yaml_data = yaml.safe_load(f.read())
-    container_tasks = yaml_data.get("tasks", []).keys()
+    container_config = container.container_config
+    container_tasks = container_config.get("tasks", []).keys()
+
     if "prepare" in container_tasks and "sanity_check" in container_tasks:
         return "data-prep-container"
     elif "infer" in container_tasks:
