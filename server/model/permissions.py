91ea3c43d1429cc2212e26f02fcf74e67307a0fc
hasan7n <hasankassim7@hotmail.com>
2026-02-23 03:49:25 +0100

    fix server permission

--- server/model/permissions.py
+++ server/model/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 from .models import Model
+from asset.models import Asset
+from mlcube.models import MlCube
 
 
 class IsAdmin(BasePermission):
@@ -28,17 +30,37 @@ class IsModelOwner(BasePermission):
 
 
 class IsAssetOrContainerOwner(BasePermission):
+    def get_asset(self, asset_id):
+        try:
+            return Asset.objects.get(id=asset_id)
+        except Asset.DoesNotExist:
+            return None
+
+    def get_container(self, container_id):
+        try:
+            return MlCube.objects.get(id=container_id)
+        except MlCube.DoesNotExist:
+            return None
+
     def has_permission(self, request, view):
         asset_id = request.data.get("asset", None)
         container_id = request.data.get("container", None)
 
         if asset_id:
-            return Model.objects.filter(
-                asset__id=asset_id, asset__owner=request.user
-            ).exists()
+            asset = self.get_asset(asset_id)
+            if not asset:
+                return False
+            if asset.owner.id == request.user.id:
+                return True
+            else:
+                return False
         elif container_id:
-            return Model.objects.filter(
-                container__id=container_id, container__owner=request.user
-            ).exists()
+            container = self.get_container(container_id)
+            if not container:
+                return False
+            if container.owner.id == request.user.id:
+                return True
+            else:
+                return False
         else:
             return False
