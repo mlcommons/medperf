1bcd289279c6f53ca501c98aa7f11f836f5e75be
hasan7n <hasankassim7@hotmail.com>
2026-03-09 20:48:30 +0100

    fix circular imports for signing assocs

--- cli/medperf/commands/association/sign.py
+++ cli/medperf/commands/association/sign.py
@@ -1,10 +1,37 @@
 from medperf.entities.benchmark import Benchmark
-from medperf.commands.association.utils import (
-    sign_dataset_association,
-    sign_model_association,
-)
+from medperf.entities.dataset import Dataset
+from medperf.entities.model import Model
+from medperf.encryption import Signing
+from medperf.commands.certificate.utils import load_user_private_key
 from medperf.account_management.account_management import get_medperf_user_data
 from medperf.exceptions import MedperfException
+from medperf import config
+
+
+def sign_dataset_association(assoc):
+    dataset_id = assoc["dataset"]
+    benchmak_id = assoc["benchmark"]
+    dataset = Dataset.get(dataset_id)
+    dataset_hash = dataset.generated_uid
+    user_private_key = load_user_private_key()
+    signature = Signing().sign_prehashed(user_private_key, dataset_hash)
+    body = {"signature": signature}
+    config.comms.update_benchmark_dataset_association(benchmak_id, dataset_id, body)
+
+
+def sign_model_association(assoc):
+    model_id = assoc["model"]
+    benchmark_id = assoc["benchmark"]
+    model = Model.get(model_id)
+    if model.is_container():
+        model_hash = model.container.image_hash
+        model_hash = model_hash.replace("sha256:", "")
+    else:
+        model_hash = model.asset.asset_hash
+    user_private_key = load_user_private_key()
+    signature = Signing().sign_prehashed(user_private_key, model_hash)
+    body = {"signature": signature}
+    config.comms.update_benchmark_model_association(benchmark_id, model_id, body)
 
 
 class SignAssociations:
