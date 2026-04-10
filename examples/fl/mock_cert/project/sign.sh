57aa56ea3dcac296666909daec8e4f6b0384f31a
Hasan Kassem <78664424+hasan7n@users.noreply.github.com>
2026-04-10 21:51:23 +0200

    Update codespaces, docs, test ca (#682)
    * fix mkdocs dependency issue
    
    * fix codespaces issue
    
    * update cert test container
    
    * add training documentation

--- examples/fl/mock_cert/project/sign.sh
+++ examples/fl/mock_cert/project/sign.sh
@@ -17,6 +17,10 @@ if [ -z "$MEDPERF_INPUT_CN" ]; then
     exit 1
 fi
 
+if [ -z "$MEDPERF_KEY_TYPE" ]; then
+    MEDPERF_KEY_TYPE="RSA"
+fi
+
 mkdir -p $OUT
 cp /project/csr.conf $OUT/
 cp -r /project/ca $OUT/
@@ -27,7 +31,19 @@ CA_CERT=$OUT/ca/cert/root.crt
 sed -i "/^commonName = /c\commonName = $MEDPERF_INPUT_CN" $CSR_CONF
 sed -i "/^DNS\.1 = /c\DNS.1 = $MEDPERF_INPUT_CN" $CSR_CONF
 
-openssl genpkey -algorithm RSA -out $OUT/key.key -pkeyopt rsa_keygen_bits:3072
+if [ "$MEDPERF_KEY_TYPE" = "RSA" ]; then
+    openssl genpkey -algorithm RSA -out $OUT/key.key -pkeyopt rsa_keygen_bits:3072
+elif [ "$MEDPERF_KEY_TYPE" = "EC" ]; then
+    # Remove default_bits line
+    sed -i '/^default_bits/d' $CSR_CONF
+    # Fix keyUsage only in v3_client section
+    sed -i '/\[ v3_client \]/,/\[ v3_server \]/{s/keyUsage = critical,digitalSignature,keyEncipherment/keyUsage = critical,digitalSignature,keyAgreement/}' $CSR_CONF
+    openssl genpkey -algorithm EC -out $OUT/key.key -pkeyopt ec_paramgen_curve:P-384
+else
+    echo "Unsupported key type: $MEDPERF_KEY_TYPE"
+    exit 1
+fi
+
 openssl req -new -key $OUT/key.key -out $OUT/csr.csr -config $CSR_CONF -extensions $EXT
 openssl x509 -req -in $OUT/csr.csr -CA $CA_CERT -CAkey $CA_KEY \
     -CAcreateserial -out $OUT/crt.crt -days 36500 -sha384 -extensions ${EXT}_crt -extfile $CSR_CONF
