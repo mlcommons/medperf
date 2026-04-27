367550672659e485a26fcc674b0eba48e50728a4
mhmdk0 <mohammadkassim54.mk@gmail.com>
2026-04-27 14:03:43 +0300

    fix codeql errors

--- cli/medperf/dashboard/utils.py
+++ cli/medperf/dashboard/utils.py
@@ -5,13 +5,13 @@ from medperf.utils import sanitize_path
 
 def get_reports_path(out_path, benchmark_id):
     """Directory used to store and read dashboard CSV artifacts for a benchmark."""
-    return os.path.join(str(out_path), str(benchmark_id))
+    full_path = os.path.join(str(out_path), str(benchmark_id))
+    return sanitize_path(full_path)
 
 
 def get_institution_from_email(email, user2institution):
     """Resolve institution label from user email using the institutions CSV mapping."""
-    full_path = user2institution.get(email, email)
-    return sanitize_path(full_path)
+    return user2institution.get(email, email)
 
 
 def stage_id2name(stage_key, stages_df):
